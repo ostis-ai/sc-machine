e0b4027afc7f647b50db57b55107ed87508e65d5
Denis Koronchik <denis.koronchik@gmail.com>
2013-11-26 00:32:33 +0300

    improve scripts

--- scripts/clean_all.sh
+++ scripts/clean_all.sh
@@ -1,9 +1,20 @@
+echo -en '\E[47;31m'"\033[1mClean sc-memory\033[0m\n"
+tput sgr0
 cd ../sc-memory
 make clean
 
+echo -en '\E[47;31m'"\033[1mClean KPM modules\033[0m\n"
+tput sgr0
+cd ../sc-kpm
+make clean
+
+echo -en '\E[47;31m'"\033[1mClean sctp server\033[0m\n"
+tput sgr0
 cd ../sc-network/cpp
 make clean
 
+echo -en '\E[47;31m'"\033[1mClean builder\033[0m\n"
+tput sgr0
 cd ../../tools/builder
 make clean
-
+~                       
--- scripts/make_all.sh
+++ scripts/make_all.sh
@@ -1,11 +1,23 @@
+echo -en '\E[47;31m'"\033[1mBuild sc-memory\033[0m\n"
+tput sgr0
 cd ../sc-memory
 qmake sc-memory.pro
 make
 
+echo -en '\E[47;31m'"\033[1mBuild KPM modules\033[0m\n"
+tput sgr0
+cd ../sc-kpm
+qmake sc_kpm.pro
+make
+
+echo -en '\E[47;31m'"\033[1mBuild sctp server\033[0m\n"
+tput sgr0
 cd ../sc-network/cpp
 qmake sctp.pro
 make
 
+echo -en '\E[47;31m'"\033[1mBuild builder\033[0m\n"
+tput sgr0
 cd ../../tools/builder
 qmake builder.pro
 make
