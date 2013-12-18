5ece19710be824d98386f3e22007212be43de327
Denis Koronchik <denis.koronchik@gmail.com>
2013-12-19 00:21:41 +0300

    add missed dependencies

--- scripts/install_deps_ubuntu.sh
+++ scripts/install_deps_ubuntu.sh
@@ -1,1 +1,1 @@
-sudo apt-get install libglib2.0-dev libantlr3c-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev make cmake antlr redis-server libhiredis-dev
+sudo apt-get install libglib2.0-dev libantlr3c-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev make cmake antlr redis-server libhiredis-dev gcc g++
