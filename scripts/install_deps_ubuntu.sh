748dd2ebb5e019af0a5c0fb320a43f9fb135fc7e
Denis Koronchik <denis.koronchik@gmail.com>
2013-12-19 00:11:56 +0300

    improve dependency install script

--- scripts/install_deps_ubuntu.sh
+++ scripts/install_deps_ubuntu.sh
@@ -1,1 +1,1 @@
-sudo apt-get install libglib2.0-dev libantlr3c-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev qt4-qmake make antlr
+sudo apt-get install libglib2.0-dev libantlr3c-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev qt4-qmake make antlr redis-server libhiredis-dev
