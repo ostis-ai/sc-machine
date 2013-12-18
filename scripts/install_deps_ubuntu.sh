f42db676436a84ff7e100d6e3905408dc8127c43
Denis Koronchik <denis.koronchik@gmail.com>
2013-12-18 23:25:10 +0200

    Update install_deps_ubuntu.sh

--- scripts/install_deps_ubuntu.sh
+++ scripts/install_deps_ubuntu.sh
@@ -1,1 +1,1 @@
-sudo apt-get install libglib2.0-dev qt4-qmake libantlr3c-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev make cmake antlr redis-server libhiredis-dev gcc g++
+sudo apt-get install libglib2.0-dev qt4-qmake qt4-dev-tools libantlr3c-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev make cmake antlr redis-server libhiredis-dev gcc g++
