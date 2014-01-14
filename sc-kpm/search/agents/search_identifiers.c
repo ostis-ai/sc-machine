502cef54d93d3fd533f9d457cb4b81374f9e34ff
ShunkevichDV <shu.dv@tut.by>
2014-01-14 21:35:29 +0300

    search identifiers - system_element checking added

--- sc-kpm/search/agents/search_identifiers.c
+++ sc-kpm/search/agents/search_identifiers.c
@@ -22,6 +22,7 @@ along with OSTIS. If not, see <http://www.gnu.org/licenses/>.
 #include "search_identifiers.h"
 #include "search_keynodes.h"
 #include "search_utils.h"
+#include "search_defines.h"
 
 #include <sc_helper.h>
 #include <sc_memory_headers.h>
@@ -30,8 +31,9 @@ sc_result agent_search_all_identifiers(sc_event *event, sc_addr arg)
 {
     sc_addr question, answer;
     sc_iterator3 *it1, *it2;
-    sc_iterator5 *it5;//, *it5Check;
-    sc_bool found = SC_FALSE;
+    sc_iterator5 *it5;
+    //sc_bool found = SC_FALSE;
+    sc_bool sys_off = SC_TRUE;
 
     if (!sc_memory_get_arc_end(arg, &question))
         return SC_RESULT_ERROR_INVALID_PARAMS;
@@ -46,7 +48,9 @@ sc_result agent_search_all_identifiers(sc_event *event, sc_addr arg)
     it1 = sc_iterator3_f_a_a_new(question, sc_type_arc_pos_const_perm, 0);
     if (sc_iterator3_next(it1) == SC_TRUE)
     {
-        found = SC_TRUE;
+        if (IS_SYSTEM_ELEMENT(sc_iterator3_value(it1, 2)))
+            sys_off = SC_FALSE;
+
         appendIntoAnswer(answer, sc_iterator3_value(it1, 2));
 
         // iterate all const arcs, that are no accessory, and go out from sc-element
@@ -57,26 +61,14 @@ sc_result agent_search_all_identifiers(sc_event *event, sc_addr arg)
                                          sc_type_node | sc_type_const | sc_type_node_norole);
         while (sc_iterator5_next(it5) == SC_TRUE)
         {
-            /*// check if this arc is an identification
-            it5Check = sc_iterator5_f_a_f_a_f_new(keynode_nrel_identification,
-                                                  sc_type_arc_common | sc_type_const,
-                                                  sc_iterator5_value(it5, 4),
-                                                  sc_type_arc_pos_const_perm,
-                                                  keynode_set_nrel_inclusion);
-
-            if (sc_iterator5_next(it5Check) == SC_TRUE)
-            {
-                // append into result
-                appendIntoAnswer(answer, sc_iterator5_value(it5, 1));
-                appendIntoAnswer(answer, sc_iterator5_value(it5, 2));
-                appendIntoAnswer(answer, sc_iterator5_value(it5, 3));
-                appendIntoAnswer(answer, sc_iterator5_value(it5, 4));
-            }
-            sc_iterator5_free(it5Check);*/
-
             // check if this relation is an identification
             if (sc_helper_check_arc(keynode_identification_relation, sc_iterator5_value(it5, 4), sc_type_arc_pos_const_perm) == SC_TRUE)
             {
+                if (sys_off == SC_TRUE && (IS_SYSTEM_ELEMENT(sc_iterator5_value(it5, 1))
+                                           || IS_SYSTEM_ELEMENT(sc_iterator5_value(it5, 2))
+                                           || IS_SYSTEM_ELEMENT(sc_iterator5_value(it5, 3))
+                                           || IS_SYSTEM_ELEMENT(sc_iterator5_value(it5, 4))))
+                    continue;
 
                 // iterate input arcs for sc-link
                 it2 = sc_iterator3_a_a_f_new(sc_type_node | sc_type_const,
@@ -86,6 +78,10 @@ sc_result agent_search_all_identifiers(sc_event *event, sc_addr arg)
                 {
                     if (sc_helper_check_arc(keynode_languages, sc_iterator3_value(it2, 0), sc_type_arc_pos_const_perm) == SC_TRUE)
                     {
+                        if (sys_off == SC_TRUE && (IS_SYSTEM_ELEMENT(sc_iterator3_value(it2, 0))
+                                                   || IS_SYSTEM_ELEMENT(sc_iterator3_value(it2, 0))))
+                            continue;
+
                         appendIntoAnswer(answer, sc_iterator3_value(it2, 0));
                         appendIntoAnswer(answer, sc_iterator3_value(it2, 1));
                     }
@@ -101,9 +97,6 @@ sc_result agent_search_all_identifiers(sc_event *event, sc_addr arg)
         sc_iterator5_free(it5);
     }
     sc_iterator3_free(it1);
-
-    if (found == SC_TRUE)
-        appendIntoAnswer(answer, keynode_nrel_identification);
 
     connect_answer_to_question(question, answer);
     finish_question(question);
