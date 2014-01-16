a277a6b85f565ee7c50da04c3cfe2d4bbd763321
Denis Koronchik <denis.koronchik@gmail.com>
2014-01-16 16:12:19 +0300

    fix date in license; add merge module of kpm; add exceptions implementation for builder

--- sc-kpm/merge/merge.h
+++ sc-kpm/merge/merge.h
@@ -20,12 +20,15 @@ along with OSTIS. If not, see <http://www.gnu.org/licenses/>.
 -----------------------------------------------------------------------------
  */
 
-#ifndef _search_defines_h_
-#define _search_defines_h_
+#ifndef _merge_h_
+#define _merge_h_
 
-#define SYSTEM_ELEMENT(el) if (sc_helper_check_arc(keynode_system_element,el, sc_type_arc_pos_const_perm) == SC_FALSE) \
-                                sc_memory_arc_new(sc_type_arc_pos_const_perm, keynode_system_element, el);
+#include "sc_memory.h"
 
-#define IS_SYSTEM_ELEMENT(el) (sc_helper_check_arc(keynode_system_element, el, sc_type_arc_pos_const_perm) == SC_TRUE)
+//! Module initialization function
+sc_result initialize();
 
-#endif // SEARCH_DEFINES_H
+//! Module shutdown function
+sc_result shutdown();
+
+#endif
--- tools/builder/src/scs_translator.cpp
+++ tools/builder/src/scs_translator.cpp
@@ -1,3 +1,25 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OSTIS (Open Semantic Technology for Intelligent Systems)
+For the latest info, see http://www.ostis.net
+
+Copyright (c) 2010-2014 OSTIS
+
+OSTIS is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OSTIS is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with OSTIS.  If not, see <http://www.gnu.org/licenses/>.
+-----------------------------------------------------------------------------
+*/
+
 #include "scs_translator.h"
 #include "utils.h"
 
@@ -317,6 +339,10 @@ void SCsTranslator::processSentenceAssign(pANTLR3_BASE_TREE node)
 
     pANTLR3_BASE_TREE node_left = (pANTLR3_BASE_TREE)node->getChild(node, 0);
     pANTLR3_BASE_TREE node_right = (pANTLR3_BASE_TREE)node->getChild(node, 1);
+
+    pANTLR3_COMMON_TOKEN tok_left = node_left->getToken(node_left);
+    pANTLR3_COMMON_TOKEN tok_right = node_left->getToken(node_left);
+    assert(tok_left && tok_right);
 
     mAssignments[GET_NODE_TEXT(node_left)] = GET_NODE_TEXT(node_right);
 }
