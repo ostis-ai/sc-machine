6fcb94a5b00c49c0b6ac90f3793c6a5d5a943501
Denis Koronchik <denis.koronchik@gmail.com>
2014-01-24 01:08:56 +0300

    change redis-server installation to 2.9.0

--- scripts/install_deps_ubuntu.sh
+++ scripts/install_deps_ubuntu.sh
@@ -1,1 +1,1 @@
-sudo apt-get install libglib2.0-dev qt4-qmake libqt4-dev libantlr3c-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev make cmake antlr antlr3 redis-server libhiredis-dev gcc g++
+sudo apt-get install libglib2.0-dev qt4-qmake libqt4-dev libantlr3c-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev make cmake antlr antlr3 libhiredis-dev gcc g++
