ca0c9c8797d80ef3e3a88ed80a8109626b34e524
Denis Koronchik <denis.koronchik@gmail.com>
2014-10-28 23:41:20 +0300

    [scripts] Fix redis script to call requested command with sudo

--- scripts/install_redis_ubuntu.sh
+++ scripts/install_redis_ubuntu.sh
@@ -2,10 +2,10 @@ wget http://download.redis.io/releases/redis-2.8.4.tar.gz
 tar xzf redis-2.8.4.tar.gz
 cd redis-2.8.4
 make
-make install
+sudo ake install
 
 echo "Create upstart configuration file for redis server"
-cat <<EOF > /etc/init/redis-server.conf
+sudo cat <<EOF > /etc/init/redis-server.conf
 start on runlevel [2345]
 stop on runlevel [016]
 
@@ -15,4 +15,4 @@ script
 end script
 EOF
 
-start redis-server
+sudo start redis-server
