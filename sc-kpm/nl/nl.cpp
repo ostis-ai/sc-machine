8f979bfe79053fc50fad1eeb341fdb1c2496cff1
Denis Koronchik <denis.koronchik@gmail.com>
2016-06-24 01:04:50 +0300

    [kpm] Add natural language module

---
+++ sc-kpm/nl/nl.cpp
@@ -0,0 +1,18 @@
+/*
+* This source file is part of an OSTIS project. For the latest info, see http://ostis.net
+* Distributed under the MIT License
+* (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
+*/
+
+#include "nl.hpp"
+
+
+sc_result nlModule::initializeImpl()
+{
+	return SC_RESULT_OK;
+}
+
+sc_result nlModule::shutdownImpl()
+{
+	return SC_RESULT_OK;
+}
---
+++ sc-kpm/nl/nl.hpp
@@ -0,0 +1,21 @@
+/*
+* This source file is part of an OSTIS project. For the latest info, see http://ostis.net
+* Distributed under the MIT License
+* (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
+*/
+
+#pragma once
+
+#include "sc_memory.h"
+#include "wrap/sc_module.hpp"
+
+#include "nl.generated.hpp"
+
+class nlModule : public ScModule
+{
+	SC_CLASS(LoadPriority(11))
+	SC_GENERATED_BODY()
+
+	sc_result initializeImpl();
+	sc_result shutdownImpl();
+};
