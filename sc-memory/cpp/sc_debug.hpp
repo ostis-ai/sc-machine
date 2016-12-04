c2b449a4c5bf88776595adbb27f37c6f23ea0353
Denis Koronchik <denis.koronchik@gmail.com>
2016-12-04 16:53:28 +0300

    [cpp] Add missed license

--- sc-memory/cpp/sc_debug.hpp
+++ sc-memory/cpp/sc_debug.hpp
@@ -1,3 +1,9 @@
+/*
+* This source file is part of an OSTIS project. For the latest info, see http://ostis.net
+* Distributed under the MIT License
+* (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
+*/
+
 #pragma once
 
 #include "utils/sc_log.hpp"
@@ -111,8 +117,6 @@ public:
 			THROW_EXCEPTION(::utils::AssertException, _message, __FILE__, __LINE__); \
 		} \
 	} while (false);	// stop runtime
-
-
 
 
 } // namespace utils
