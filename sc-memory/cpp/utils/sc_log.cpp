a2b3e2503ea46eed64704ddf4f45741e0759eb8d
Denis Koronchik <denis.koronchik@gmail.com>
2016-12-19 00:54:58 +0300

    [cpp] Add missed file license

--- sc-memory/cpp/utils/sc_log.cpp
+++ sc-memory/cpp/utils/sc_log.cpp
@@ -1,3 +1,9 @@
+/*
+* This source file is part of an OSTIS project. For the latest info, see http://ostis.net
+* Distributed under the MIT License
+* (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
+*/
+
 #include "sc_log.hpp"
 #include "../sc_debug.hpp"
 
