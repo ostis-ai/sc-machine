766585eb42eb54360afdfabacf55dbcd6dd6cd0f
shunkevichdv <shunkevichdv@gmail.com>
2017-03-03 14:25:56 +0300

    Open valve agent added

---
+++ sc-kpm/bottle/AOpenValveAgent.cpp
@@ -0,0 +1,38 @@
+/*
+* This source file is part of an OSTIS project. For the latest info, see http://ostis.net
+* Distributed under the MIT License
+* (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
+*/
+
+#include "AOpenValveAgent.hpp"
+
+#include <iostream>
+
+namespace bottle
+{
+
+SC_AGENT_IMPLEMENTATION(AOpenValveAgent)
+{
+    if (!edgeAddr.IsValid())
+        return SC_RESULT_ERROR;
+
+    std::cout << "Button pressed!" << std::endl;
+
+    ScAddr mybutton = ms_context->GetArcEnd(edgeAddr);
+    if (!ms_context->HelperCheckArc(Keynodes::button, mybutton, sc_type_arc_pos_const_perm))
+        return SC_RESULT_ERROR_INVALID_PARAMS;
+
+    ScIterator5Ptr iter_valve = ms_context->Iterator5(mybutton, SC_TYPE(sc_type_arc_common | sc_type_const), sc_type_node, sc_type_arc_pos_const_perm, Keynodes::nrel_equipment_connection);
+    while (iter_valve->Next())
+    {
+        ScAddr myvalve = iter_valve->Get(2);
+        if (ms_context->HelperCheckArc(Keynodes::valve, myvalve, sc_type_arc_pos_const_perm))
+            ms_context->CreateArc(sc_type_arc_pos_const_perm, Keynodes::opened, myvalve);
+
+        std::cout << "Valve opened!" << std::endl;
+    }
+
+    return SC_RESULT_OK;
+}
+
+} // namespace bottle
