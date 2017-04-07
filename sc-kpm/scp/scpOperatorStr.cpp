b39053c4ec929005758ae0f39bf6ef4d39843415
shunkevichdv <shunkevichdv@gmail.com>
2017-04-07 11:37:17 +0300

    SCP Operator class with 1,3,5 operands implemented

--- sc-kpm/scp/scpOperatorStr.cpp
+++ sc-kpm/scp/scpOperatorStr.cpp
@@ -9,13 +9,16 @@
 #include "scpUtils.hpp"
 
 #include "scpOperatorStr.hpp"
+#include <iostream>
+#include <sstream>
+
 
 namespace scp
 {
 
-SCPOperatorStr1::SCPOperatorStr1(ScMemoryContext &ctx, ScAddr addr): SCPOperator(ctx, addr)
+SCPOperatorStr1::SCPOperatorStr1(ScMemoryContext &ctx_, ScAddr addr_): SCPOperator(ctx_, addr_)
 {
-
+    operands = std::vector<SCPOperand*>(1);
 }
 
 sc_result SCPOperatorStr1::Parse()
@@ -27,7 +30,12 @@ sc_result SCPOperatorStr1::Parse()
         SCPOperand *operand = new SCPOperand(ms_context, iter_operator->Get(1));
         if (!(operand->GetOrder() == 1 && operands[0] == nullptr))
         {
-            Utils::logSCPError(ms_context, "Invalid operand order " + operand->GetOrder(), addr);
+#ifdef SCP_DEBUG
+            std::stringstream ss;
+            ss << "Invalid operand order ";
+            ss << (int)operand->GetOrder();
+            Utils::logSCPError(ms_context, ss.str(), addr);
+#endif
             return SC_RESULT_ERROR_INVALID_PARAMS;
         }
         else
@@ -38,10 +46,11 @@ sc_result SCPOperatorStr1::Parse()
     return SC_RESULT_OK;
 }
 
-SCPOperatorStr3::SCPOperatorStr3(ScMemoryContext &ctx, ScAddr addr): SCPOperator(ctx, addr)
+SCPOperatorStr3::SCPOperatorStr3(ScMemoryContext &ctx_, ScAddr addr_): SCPOperator(ctx_, addr_)
 {
-
+    operands = std::vector<SCPOperand*>(3);
 }
+
 
 sc_result SCPOperatorStr3::Parse()
 {
@@ -52,7 +61,12 @@ sc_result SCPOperatorStr3::Parse()
         SCPOperand *operand = new SCPOperand(ms_context, iter_operator->Get(1));
         if (!(operand->GetOrder() > 0 && operand->GetOrder() < 4 && operands[operand->GetOrder() - 1] == nullptr))
         {
-            Utils::logSCPError(ms_context, "Invalid operand order " + operand->GetOrder(), addr);
+#ifdef SCP_DEBUG
+            std::stringstream ss;
+            ss << "Invalid operand order ";
+            ss << (int)operand->GetOrder();
+            Utils::logSCPError(ms_context, ss.str(), addr);
+#endif
             return SC_RESULT_ERROR_INVALID_PARAMS;
         }
         else
@@ -63,10 +77,11 @@ sc_result SCPOperatorStr3::Parse()
     return SC_RESULT_OK;
 }
 
-SCPOperatorStr5::SCPOperatorStr5(ScMemoryContext &ctx, ScAddr addr): SCPOperator(ctx, addr)
+SCPOperatorStr5::SCPOperatorStr5(ScMemoryContext &ctx_, ScAddr addr_): SCPOperator(ctx_, addr_)
 {
-
+    operands = std::vector<SCPOperand*>(5);
 }
+
 
 sc_result SCPOperatorStr5::Parse()
 {
@@ -77,7 +92,12 @@ sc_result SCPOperatorStr5::Parse()
         SCPOperand *operand = new SCPOperand(ms_context, iter_operator->Get(1));
         if (!(operand->GetOrder() > 0 && operand->GetOrder() < 6 && operands[operand->GetOrder() - 1] == nullptr))
         {
-            Utils::logSCPError(ms_context, "Invalid operand order " + operand->GetOrder(), addr);
+#ifdef SCP_DEBUG
+            std::stringstream ss;
+            ss << "Invalid operand order ";
+            ss << (int)operand->GetOrder();
+            Utils::logSCPError(ms_context, ss.str(), addr);
+#endif
             return SC_RESULT_ERROR_INVALID_PARAMS;
         }
         else
--- sc-kpm/scp/scpOperatorStr.hpp
+++ sc-kpm/scp/scpOperatorStr.hpp
@@ -4,6 +4,8 @@
 * (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
 */
 
+#pragma once
+
 #include "sc-memory/cpp/sc_addr.hpp"
 #include "scpKeynodes.hpp"
 #include "scpOperand.hpp"
@@ -14,31 +16,22 @@
 namespace scp
 {
 
-class SCPOperatorStr1: SCPOperator
+class SCPOperatorStr1: public SCPOperator
 {
-protected:
-    std::vector<SCPOperand*> operands = std::vector<SCPOperand*>(1);
-
 public:
     SCPOperatorStr1(ScMemoryContext &ctx, ScAddr addr);
     sc_result Parse();
 };
 
-class SCPOperatorStr3: SCPOperator
+class SCPOperatorStr3: public SCPOperator
 {
-protected:
-    std::vector<SCPOperand*> operands = std::vector<SCPOperand*>(3);
-
 public:
     SCPOperatorStr3(ScMemoryContext &ctx, ScAddr addr);
     sc_result Parse();
 };
 
-class SCPOperatorStr5: SCPOperator
+class SCPOperatorStr5: public SCPOperator
 {
-protected:
-    std::vector<SCPOperand*> operands = std::vector<SCPOperand*>(5);
-
 public:
     SCPOperatorStr5(ScMemoryContext &ctx, ScAddr addr);
     sc_result Parse();
