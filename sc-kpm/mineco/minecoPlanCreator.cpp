0baee2f251e6b841bbaa3e6bd2bf3e5307ddc939
shunkevichdv <shunkevichdv@gmail.com>
2017-05-03 13:13:36 +0300

    Plan creation agent updated

--- sc-kpm/mineco/minecoPlanCreator.cpp
+++ sc-kpm/mineco/minecoPlanCreator.cpp
@@ -12,9 +12,21 @@
 namespace mineco
 {
 ScAddr AMinEcoPlanCreator::msAgentKeynode;
+ScAddr AMinEcoPlanCreator::rrel_1;
+ScAddr AMinEcoPlanCreator::rrel_2;
+ScAddr AMinEcoPlanCreator::concept_organization;
+ScAddr AMinEcoPlanCreator::ap_receiving_and_registering_book;
+ScAddr AMinEcoPlanCreator::concept_store_opening;
 ScAddr AMinEcoPlanCreator::question_plan_creation;
 ScAddr AMinEcoPlanCreator::question_initiated;
 ScAddr AMinEcoPlanCreator::nrel_answer;
+ScAddr AMinEcoPlanCreator::nrel_goto;
+ScAddr AMinEcoPlanCreator::nrel_decomposition_of_action;
+ScAddr AMinEcoPlanCreator::nrel_address;
+ScAddr AMinEcoPlanCreator::nrel_legal_address;
+ScAddr AMinEcoPlanCreator::nrel_administrative_subordination;
+ScAddr AMinEcoPlanCreator::nrel_registration;
+ScAddr AMinEcoPlanCreator::inspection_MNS;
 
 SC_AGENT_IMPLEMENTATION(AMinEcoPlanCreator)
 {
@@ -26,49 +38,22 @@ SC_AGENT_IMPLEMENTATION(AMinEcoPlanCreator)
     if (!ms_context->HelperCheckArc(question_plan_creation, action, sc_type_arc_pos_const_perm))
         return SC_RESULT_ERROR_INVALID_PARAMS;
 
-    ScAddr program;
-    ScIterator3Ptr iter_param = ms_context->Iterator3(action, sc_type_arc_pos_const_perm, sc_type_node);
+    ScAddr program, param;
+    ScIterator5Ptr iter_param = ms_context->Iterator5(action, sc_type_arc_pos_const_perm, sc_type_node, sc_type_arc_pos_const_perm, rrel_1);
     if (iter_param->Next())
         program = iter_param->Get(2);
+    else
+        return SC_RESULT_ERROR_INVALID_PARAMS;
+
+    iter_param = ms_context->Iterator5(action, sc_type_arc_pos_const_perm, sc_type_node, sc_type_arc_pos_const_perm, rrel_2);
+    if (iter_param->Next())
+        param = iter_param->Get(2);
     else
         return SC_RESULT_ERROR_INVALID_PARAMS;
 
     ScTemplate program_templ;
     ms_context->HelperBuildTemplate(program_templ, program);
     ScTemplateGenParams gen_params;
-
-    /*ScAddr process_node;
-    ScIterator5Ptr iter_temp = ms_context->Iterator5(Keynodes::scp_process, sc_type_arc_pos_var_perm, SC_TYPE(sc_type_var | sc_type_node), sc_type_arc_pos_const_perm, program);
-    if (iter_temp->IsValid() && iter_temp->Next())
-        process_node = iter_temp->Get(2);
-    else
-        return SC_RESULT_ERROR_INVALID_PARAMS;*/
-
-    /*iter_temp = ms_context->Iterator5(process_node, sc_type_arc_pos_var_perm, SC_TYPE(sc_type_var | sc_type_node), sc_type_arc_pos_const_perm, program);
-    if (!iter_temp->IsValid())
-        return SC_RESULT_ERROR_INVALID_PARAMS;*/
-
-    /*while (iter_temp->Next())
-    {
-        ScAddr order;
-        if (Utils::resolveOrderRoleRelation((ScMemoryContext&)ms_context, iter_temp->Get(1), order));
-        {
-            iter_param = ms_context->Iterator5(params, sc_type_arc_pos_const_perm, SC_TYPE(0), sc_type_arc_pos_const_perm, order);
-            if (!iter_param->Next())
-            {
-    #ifdef SCP_DEBUG
-                Utils::logSCPError((ScMemoryContext&)ms_context, "Missed scp-program parameter", order);
-    #endif
-                continue;
-            }
-
-            //!TODO gen_params.add(iter_temp->Get(2),iter_param->Get(2));
-            //Utils::printSystemIdentifier((ScMemoryContext&)ms_context, iter_temp->Get(2));
-            //std::cout << "-->" << std::endl;
-            //Utils::printSystemIdentifier((ScMemoryContext&)ms_context, iter_param->Get(2));
-            //std::cout << std::endl;
-        }
-    }*/
 
     ScTemplateGenResult res;
     ms_context->HelperGenTemplate(program_templ, res);
@@ -79,6 +64,107 @@ SC_AGENT_IMPLEMENTATION(AMinEcoPlanCreator)
     {
         ms_context->CreateArc(sc_type_arc_pos_const_perm, answer, res[i]);
     }
+
+    // search of actions
+    ScAddr store_opening, book_registering;
+    for (int i = 0; i < res.GetSize(); i++)
+    {
+        if (ms_context->GetElementType(res[i]).IsNode())
+        {
+
+            if (ms_context->HelperCheckArc(concept_store_opening, res[i], sc_type_arc_pos_const_perm))
+            {
+                store_opening = res[i];
+            }
+            if (ms_context->HelperCheckArc(ap_receiving_and_registering_book, res[i], sc_type_arc_pos_const_perm))
+            {
+                book_registering = res[i];
+            }
+        }
+    }
+
+    if (!store_opening.IsValid() || !book_registering.IsValid())
+        return SC_RESULT_ERROR_INVALID_PARAMS;
+
+    //Removing unnecessary actions
+    ScAddr decomp, curr_action, next_action;
+
+    ScIterator5Ptr iter_action = ms_context->Iterator5(sc_type_node, SC_TYPE(sc_type_arc_common | sc_type_const), store_opening, sc_type_arc_pos_const_perm, nrel_decomposition_of_action);
+    if (iter_action->Next())
+        decomp = iter_action->Get(0);
+    else
+        return SC_RESULT_ERROR_INVALID_PARAMS;
+
+    iter_action = ms_context->Iterator5(decomp, sc_type_arc_pos_const_perm, sc_type_node, sc_type_arc_pos_const_perm, rrel_1);
+    if (iter_action->Next())
+        curr_action = iter_action->Get(2);
+    else
+        return SC_RESULT_ERROR_INVALID_PARAMS;
+
+    while (true)
+    {
+        if (ms_context->HelperCheckArc(ap_receiving_and_registering_book, curr_action, sc_type_arc_pos_const_perm))
+        {
+            ScTemplate mytempl1;
+            mytempl1.TripleWithRelation(
+                param,
+                ScType::EdgeDCommonVar,
+                ScType::Link >> "_addr1",
+                ScType::EdgeAccessVarPosPerm,
+                nrel_legal_address
+            );
+            mytempl1.TripleWithRelation(
+                ScType::NodeVar >> "_district",
+                ScType::EdgeDCommonVar,
+                "_addr1",
+                ScType::EdgeAccessVarPosPerm,
+                nrel_administrative_subordination
+            );
+
+            ScTemplateSearchResult result;
+            ms_context->HelperSearchTemplate(mytempl1, result);
+            ScAddr district;
+            if (result.Size() > 0)
+                district = result[0]["_district"];
+            else
+                break;
+
+            ScIterator5Ptr iter1 = ms_context->Iterator5(district, SC_TYPE(sc_type_arc_common | sc_type_const), SC_TYPE(0), sc_type_arc_pos_const_perm, nrel_administrative_subordination);
+            while (iter1->Next())
+            {
+                ScAddr addr2 = iter1->Get(2);
+                ScIterator5Ptr iter2 = ms_context->Iterator5(sc_type_node, SC_TYPE(sc_type_arc_common | sc_type_const), addr2, sc_type_arc_pos_const_perm, nrel_address);
+                while (iter2->Next())
+                {
+                    ScAddr imns = iter2->Get(0);;
+                    if (ms_context->HelperCheckArc(inspection_MNS, imns, sc_type_arc_pos_const_perm))
+                    {
+                        ScAddr arc1 = ms_context->CreateArc((sc_type_arc_common | sc_type_const), curr_action, imns);
+                        ScAddr arc2 = ms_context->CreateArc(sc_type_arc_pos_const_perm, nrel_registration, arc1);
+                        ms_context->CreateArc(sc_type_arc_pos_const_perm, answer, arc1);
+                        ms_context->CreateArc(sc_type_arc_pos_const_perm, answer, arc2);
+                        ms_context->CreateArc(sc_type_arc_pos_const_perm, answer, imns);
+                        ms_context->CreateArc(sc_type_arc_pos_const_perm, answer, nrel_registration);
+                    }
+                }
+            }
+            break;
+        }
+        iter_action = ms_context->Iterator5(curr_action, SC_TYPE(sc_type_arc_common | sc_type_const), sc_type_node, sc_type_arc_pos_const_perm, nrel_goto);
+        if (iter_action->Next())
+        {
+            next_action = iter_action->Get(2);
+            ms_context->EraseElement(curr_action);
+            curr_action = next_action;
+        }
+        else
+        {
+            ms_context->EraseElement(curr_action);
+            break;
+        }
+    }
+
+    //finish action
     ScAddr myarc = ms_context->CreateArc(SC_TYPE(sc_type_arc_common | sc_type_const), action, answer);
     ms_context->CreateArc(sc_type_arc_pos_const_perm, nrel_answer, myarc);
 
--- sc-kpm/mineco/minecoPlanCreator.hpp
+++ sc-kpm/mineco/minecoPlanCreator.hpp
@@ -24,6 +24,22 @@ public:
     SC_PROPERTY(Keynode("sc_agent_of_mineco_plan_creation"), ForceCreate)
     static ScAddr msAgentKeynode;
 
+private:
+    SC_PROPERTY(Keynode("rrel_1"), ForceCreate)
+    static ScAddr rrel_1;
+
+    SC_PROPERTY(Keynode("rrel_2"), ForceCreate)
+    static ScAddr rrel_2;
+
+    SC_PROPERTY(Keynode("concept_organization"), ForceCreate)
+    static ScAddr concept_organization;
+
+    SC_PROPERTY(Keynode("administrative_procedure.receiving_and_registering_book_of_comments_and_suggestions"), ForceCreate)
+    static ScAddr ap_receiving_and_registering_book;
+
+    SC_PROPERTY(Keynode("concept_store_opening"), ForceCreate)
+    static ScAddr concept_store_opening;
+
     SC_PROPERTY(Keynode("question_plan_creation"), ForceCreate)
     static ScAddr question_plan_creation;
 
@@ -33,6 +49,28 @@ public:
     SC_PROPERTY(Keynode("nrel_answer"), ForceCreate)
     static ScAddr nrel_answer;
 
+    SC_PROPERTY(Keynode("nrel_goto"), ForceCreate)
+    static ScAddr nrel_goto;
+
+    SC_PROPERTY(Keynode("nrel_decomposition_of_action"), ForceCreate)
+    static ScAddr nrel_decomposition_of_action;
+
+    SC_PROPERTY(Keynode("nrel_address"), ForceCreate)
+    static ScAddr nrel_address;
+
+    SC_PROPERTY(Keynode("nrel_legal_address"), ForceCreate)
+    static ScAddr nrel_legal_address;
+
+    SC_PROPERTY(Keynode("nrel_administrative_subordination"), ForceCreate)
+    static ScAddr nrel_administrative_subordination;
+
+    SC_PROPERTY(Keynode("nrel_registration"), ForceCreate)
+    static ScAddr nrel_registration;
+
+    SC_PROPERTY(Keynode("inspection_MNS"), ForceCreate)
+    static ScAddr inspection_MNS;
+
+    void AddRegistrationPlace(ScAddr action, ScAddr organization, ScAddr answer);
 };
 
 }
