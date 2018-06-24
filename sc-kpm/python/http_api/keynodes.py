4df92413c6ae6c75bcf437bc801a2b98e37d82da
Denis Koronchik <denis.koronchik@gmail.com>
2018-06-24 20:03:28 +0300

    [python] Cleanup python code

--- sc-kpm/python/http_api/keynodes.py
+++ sc-kpm/python/http_api/keynodes.py
@@ -1,44 +1,48 @@
-import threading, functools
+import threading
+import functools
 
 from sc import *
 
+
 def synchronized(lock=None):
-    def _decorator(wrapped):
-        @functools.wraps(wrapped)
-        def _wrapper(*args, **kwargs):
-            with lock:
-                return wrapped(*args, **kwargs)
-        return _wrapper
-    return _decorator
+  def _decorator(wrapped):
+    @functools.wraps(wrapped)
+    def _wrapper(*args, **kwargs):
+      with lock:
+        return wrapped(*args, **kwargs)
+    return _wrapper
+  return _decorator
+
 
 keynodesLock = threading.Lock()
 
+
 class Keynodes:
 
-    resolved = {}
+  resolved = {}
 
-    NrelMimeType = 'nrel_mimetype'
-    NrelFormat = 'nrel_format'
+  NrelMimeType = 'nrel_mimetype'
+  NrelFormat = 'nrel_format'
 
-    @staticmethod
-    @synchronized(keynodesLock)
-    def Get(key):
-        try:
-            return Keynodes.resolved[key]
-        except:
-            return ScAddr()
+  @staticmethod
+  @synchronized(keynodesLock)
+  def Get(key):
+    try:
+      return Keynodes.resolved[key]
+    except:
+      return ScAddr()
 
-    @staticmethod
-    @synchronized(keynodesLock)
-    def Init(ctx):
-        keynodesList = [
-            Keynodes.NrelMimeType,
-            Keynodes.NrelFormat
-        ]
+  @staticmethod
+  @synchronized(keynodesLock)
+  def Init(ctx):
+    keynodesList = [
+        Keynodes.NrelMimeType,
+        Keynodes.NrelFormat
+    ]
 
-        for k in keynodesList:
-            addr = ctx.HelperResolveSystemIdtf(k, ScType.Unknown)
-            if addr.IsValid():
-                Keynodes.resolved[k] = addr
+    for k in keynodesList:
+      addr = ctx.HelperResolveSystemIdtf(k, ScType.Unknown)
+      if addr.IsValid():
+        Keynodes.resolved[k] = addr
 
-            print('Keynode: {} - {}'.format(k, addr.ToInt()))
+      print('Keynode: {} - {}'.format(k, addr.ToInt()))
--- sc-kpm/python/http_api/ws_sc_json.py
+++ sc-kpm/python/http_api/ws_sc_json.py
@@ -4,326 +4,330 @@ import tornado
 from tornado import websocket
 from sc import *
 
-import json, sys, traceback, threading
+import json
+import sys
+import traceback
+import threading
 
 clients = []
 
+
 class EventHandler:
 
-    def __init__(self):
-        self.evt_native = None
-        self.send_func = None
+  def __init__(self):
+    self.evt_native = None
+    self.send_func = None
 
-    def Set(self, evt, func):
-        self.evt_native = evt
-        self.send_func = func
+  def Set(self, evt, func):
+    self.evt_native = evt
+    self.send_func = func
 
-    def OnEmit(self, evt):
-        self.send_func(evt)
+  def OnEmit(self, evt):
+    self.send_func(evt)
+
 
 class ScJsonSocketHandler(websocket.WebSocketHandler):
-    
-    def initialize(self, evt_manager):
-        self.events = {}
-        self.event_manager = evt_manager
-        self.alive = False
 
-    def check_origin(self, origin):
-        return True
+  def initialize(self, evt_manager):
+    self.events = {}
+    self.event_manager = evt_manager
+    self.alive = False
 
-    def open(self):
-        if self not in clients:
-            clients.append(self)
-        self.alive = True
-        print('connected')
+  def check_origin(self, origin):
+    return True
 
-    def on_close(self):
-        if self in clients:
-            clients.remove(self)
-        print('disconnected', len(clients))
-        self.alive = False
+  def open(self):
+    if self not in clients:
+      clients.append(self)
+    self.alive = True
+    print('connected')
 
-        # remove all events
-        for eid, evt in self.events.items():
-            self.event_manager.DestroyEvent(evt.evt_native)
-        self.events.clear()
+  def on_close(self):
+    if self in clients:
+      clients.remove(self)
+    print('disconnected', len(clients))
+    self.alive = False
 
-    def on_message(self, msg):
-        params = json.loads(msg)
-        status = False
+    # remove all events
+    for eid, evt in self.events.items():
+      self.event_manager.DestroyEvent(evt.evt_native)
+    self.events.clear()
 
-        ctx = ScMemoryContext.Create(str(self))
-        try:
-            request_type = params['type']
-            request_payload = params['payload']
+  def on_message(self, msg):
+    params = json.loads(msg)
+    status = False
 
-            response_payload = None
-            if request_type == 'keynodes':
-                response_payload = self.handleKeynodes(ctx, request_payload)
-            elif request_type == 'create_elements':
-                response_payload = self.handleCreateElements(ctx, request_payload)
-            elif request_type == 'check_elements':
-                response_payload = self.handleCheckElements(ctx, request_payload)
-            elif request_type == 'delete_elements':
-                response_payload = self.handleDeleteElements(ctx, request_payload)
-            elif request_type == 'search_template':
-                response_payload = self.handleTemplateSearch(ctx, request_payload)
-            elif request_type == 'generate_template':
-                response_payload = self.handleTemplateGenerate(ctx, request_payload)
-            elif request_type == 'content':
-                response_payload = self.handleContent(ctx, request_payload)
-            elif request_type == 'events':
-                response_payload = self.handleEvents(ctx, request_payload)
+    ctx = ScMemoryContext.Create(str(self))
+    try:
+      request_type = params['type']
+      request_payload = params['payload']
 
-            if response_payload:
-                status = True
-            else:
-                response_payload = "Unsupported request type: {}".format(request_type)
-        except:
-            response_payload = sys.exc_info()[0]
-            print ("Unexpected error:", response_payload)
-            traceback.print_exc(file=sys.stdout)
-        finally:
-            pass
+      response_payload = None
+      if request_type == 'keynodes':
+        response_payload = self.handleKeynodes(ctx, request_payload)
+      elif request_type == 'create_elements':
+        response_payload = self.handleCreateElements(ctx, request_payload)
+      elif request_type == 'check_elements':
+        response_payload = self.handleCheckElements(ctx, request_payload)
+      elif request_type == 'delete_elements':
+        response_payload = self.handleDeleteElements(ctx, request_payload)
+      elif request_type == 'search_template':
+        response_payload = self.handleTemplateSearch(ctx, request_payload)
+      elif request_type == 'generate_template':
+        response_payload = self.handleTemplateGenerate(ctx, request_payload)
+      elif request_type == 'content':
+        response_payload = self.handleContent(ctx, request_payload)
+      elif request_type == 'events':
+        response_payload = self.handleEvents(ctx, request_payload)
 
-        # make and send response
-        response = {
-            'id': params['id'],
-            'event': False,
-            'status': status,
-            'payload': response_payload
-        }
+      if response_payload:
+        status = True
+      else:
+        response_payload = "Unsupported request type: {}".format(request_type)
+    except:
+      response_payload = sys.exc_info()[0]
+      print("Unexpected error:", response_payload)
+      traceback.print_exc(file=sys.stdout)
+    finally:
+      pass
 
-        self.sendMessage(json.dumps(response))
+    # make and send response
+    response = {
+        'id': params['id'],
+        'event': False,
+        'status': status,
+        'payload': response_payload
+    }
 
-    def sendMessage(self, msg):
-        self.write_message(msg)
+    self.sendMessage(json.dumps(response))
 
-    def handleKeynodes(self, ctx, payload):
-        result = [0] * len(payload)
+  def sendMessage(self, msg):
+    self.write_message(msg)
 
-        idx = 0
-        for cmd in payload:
-            cmdType = cmd['command']
-            idtf = cmd['idtf']
-            
-            if cmdType == 'find':
-                result[idx] = ctx.HelperResolveSystemIdtf(idtf, ScType.Unknown).ToInt()
-            elif cmdType == 'resolve':
-                elType = ScType(cmd['elType'])
-                result[idx] = ctx.HelperResolveSystemIdtf(idtf, elType).ToInt()
+  def handleKeynodes(self, ctx, payload):
+    result = [0] * len(payload)
 
-            idx += 1
+    idx = 0
+    for cmd in payload:
+      cmdType = cmd['command']
+      idtf = cmd['idtf']
 
-        return result
+      if cmdType == 'find':
+        result[idx] = ctx.HelperResolveSystemIdtf(idtf, ScType.Unknown).ToInt()
+      elif cmdType == 'resolve':
+        elType = ScType(cmd['elType'])
+        result[idx] = ctx.HelperResolveSystemIdtf(idtf, elType).ToInt()
 
-    def handleCreateElements(self, ctx, payload):
+      idx += 1
 
-        result = [0] * len(payload)
+    return result
 
-        idx = 0
-        for cmd in payload:
-            el = cmd['el']
-            elType = ScType(cmd['type'])
+  def handleCreateElements(self, ctx, payload):
 
-            if el == 'node':
-                result[idx] = ctx.CreateNode(elType).ToInt()
-            
-            elif el == 'edge':
-                
-                def resolveAdjAddr(obj):
-                    value = obj['value']
-                    if obj['type'] == 'ref':
-                        return ScAddr(result[value])
+    result = [0] * len(payload)
 
-                    return ScAddr(value)
+    idx = 0
+    for cmd in payload:
+      el = cmd['el']
+      elType = ScType(cmd['type'])
 
-                src = resolveAdjAddr(cmd['src'])
-                trg = resolveAdjAddr(cmd['trg'])
+      if el == 'node':
+        result[idx] = ctx.CreateNode(elType).ToInt()
 
-                result[idx] = ctx.CreateEdge(elType, src, trg).ToInt()
-                
-            elif el == 'link':
-                # TODO: support link type
-                addr = ctx.CreateLink()
-                if addr.IsValid():
-                    ctx.SetLinkContent(addr, cmd['content'])
-                result[idx] = addr.ToInt()
+      elif el == 'edge':
 
-            idx += 1
+        def resolveAdjAddr(obj):
+          value = obj['value']
+          if obj['type'] == 'ref':
+            return ScAddr(result[value])
 
-        return result
+          return ScAddr(value)
 
-    def handleCheckElements(self, ctx, payload):
-        result = [0] * len(payload)
+        src = resolveAdjAddr(cmd['src'])
+        trg = resolveAdjAddr(cmd['trg'])
 
-        idx = 0
-        for value in payload:
-            addr = ScAddr(value)
-            if addr.IsValid():
-                result[idx] = ctx.GetElementType(addr).ToInt()
+        result[idx] = ctx.CreateEdge(elType, src, trg).ToInt()
 
-            idx += 1
-        
-        return result
+      elif el == 'link':
+        # TODO: support link type
+        addr = ctx.CreateLink()
+        if addr.IsValid():
+          ctx.SetLinkContent(addr, cmd['content'])
+        result[idx] = addr.ToInt()
 
-    def handleDeleteElements(self, ctx, payload):
+      idx += 1
 
-        for value in payload:
-            ctx.DeleteElement(ScAddr(value))
+    return result
 
-        return True
+  def handleCheckElements(self, ctx, payload):
+    result = [0] * len(payload)
 
-    def makeTemplate(self, triples):
-        used_aliases = {}
+    idx = 0
+    for value in payload:
+      addr = ScAddr(value)
+      if addr.IsValid():
+        result[idx] = ctx.GetElementType(addr).ToInt()
 
-        def convert_value(value):
-            t = value['type']
-            v = value['value']
-            result = v
+      idx += 1
 
-            if t == 'type':
-                result = ScType(v)
-            elif t == 'addr':
-                result = ScAddr(v)
-            
-            if 'alias' in value:
-                alias = value['alias']
-                result = result >> alias
-                used_aliases[alias] = True
+    return result
 
-            return result
+  def handleDeleteElements(self, ctx, payload):
 
-        templ = ScTemplate()
-        for triple in triples:
-            src = convert_value(triple[0])
-            edg = convert_value(triple[1])
-            trg = convert_value(triple[2])
-            templ.Triple(src, edg, trg)
+    for value in payload:
+      ctx.DeleteElement(ScAddr(value))
 
-        return (templ, used_aliases)
+    return True
 
+  def makeTemplate(self, triples):
+    used_aliases = {}
 
-    def handleTemplateSearch(self, ctx, payload):
-        templ, used_aliases = self.makeTemplate(payload)
+    def convert_value(value):
+      t = value['type']
+      v = value['value']
+      result = v
 
-        # run search
-        search_result = ctx.HelperSearchTemplate(templ)
-        alias_num = 0
-        aliases = {}
-        for k in used_aliases.keys():
-            aliases[k] = alias_num
-            alias_num += 1
+      if t == 'type':
+        result = ScType(v)
+      elif t == 'addr':
+        result = ScAddr(v)
 
-        addrs = []
-        for idx in range(search_result.Size()):
-            items = [0] * alias_num
-            for alias_name, i in aliases.items():
-                items[i] = search_result[idx][alias_name].ToInt()
-            addrs.append(items)
+      if 'alias' in value:
+        alias = value['alias']
+        result = result >> alias
+        used_aliases[alias] = True
 
-        return {
-            'aliases': aliases,
-            'addrs': addrs
-        }
-    
-    def handleTemplateGenerate(self, ctx, payload):
-        templ, used_aliases = self.makeTemplate(payload["templ"])
+      return result
 
-        params = payload['params']
-        templ_params = ScTemplateGenParams()
-        for alias, value in params.items():
-            templ_params.Add(alias, ScAddr(value))
-        
-        # run search
-        gen_result = ctx.HelperGenTemplate(templ, templ_params)
-        if not gen_result:
-            return None
+    templ = ScTemplate()
+    for triple in triples:
+      src = convert_value(triple[0])
+      edg = convert_value(triple[1])
+      trg = convert_value(triple[2])
+      templ.Triple(src, edg, trg)
 
-        result = {}
-        for alias in used_aliases:
-            result[alias] = gen_result[alias].ToInt()
+    return (templ, used_aliases)
 
-        return result
+  def handleTemplateSearch(self, ctx, payload):
+    templ, used_aliases = self.makeTemplate(payload)
 
-    def handleContent(self, ctx, payload):
-        result = []
-        for cmd in payload:
-            t = cmd['command']
-            a = ScAddr(cmd['addr'])
+    # run search
+    search_result = ctx.HelperSearchTemplate(templ)
+    alias_num = 0
+    aliases = {}
+    for k in used_aliases.keys():
+      aliases[k] = alias_num
+      alias_num += 1
 
-            if t == 'set':
-                # TODO: support type of content
-                result.append(ctx.SetLinkContent(a, cmd['data']))
-            elif t == 'get':
-                content = ctx.GetLinkContent(a)
-                value = None
-                ctype = None
-                if content:
-                    ctype = content.GetType()
+    addrs = []
+    for idx in range(search_result.Size()):
+      items = [0] * alias_num
+      for alias_name, i in aliases.items():
+        items[i] = search_result[idx][alias_name].ToInt()
+      addrs.append(items)
 
-                    if ctype == ScLinkContent.Int:
-                        value = content.AsInt()
-                        ctype = 'int'
-                    elif ctype == ScLinkContent.Float:
-                        value = content.AsFloat()
-                        ctype = 'float'
-                    elif ctype == ScLinkContent.String:
-                        value = content.AsString() 
-                        ctype = 'string'
+    return {
+        'aliases': aliases,
+        'addrs': addrs
+    }
 
-                result.append({
-                    'value': value,
-                    'type': ctype
-                })
+  def handleTemplateGenerate(self, ctx, payload):
+    templ, used_aliases = self.makeTemplate(payload["templ"])
 
-        return result
+    params = payload['params']
+    templ_params = ScTemplateGenParams()
+    for alias, value in params.items():
+      templ_params.Add(alias, ScAddr(value))
 
-    def onEmitEvent(self, evt):
-        response = {
-            'id': evt.id,
-            'event': True,
-            'status': True,
-            'payload': [evt.addr.ToInt(), evt.edge_addr.ToInt(), evt.other_addr.ToInt()]
-        }
-        data = json.dumps(response)
-        tornado.ioloop.IOLoop.instance().add_callback(self.sendMessage, data)
+    # run search
+    gen_result = ctx.HelperGenTemplate(templ, templ_params)
+    if not gen_result:
+      return None
 
-    def handleEvents(self, ctx, payload):
-        result = []
-        createEvents = None
-        try:
-            createEvents = payload['create']
-        except KeyError:
-            pass
+    result = {}
+    for alias in used_aliases:
+      result[alias] = gen_result[alias].ToInt()
 
-        if createEvents:
-            for evt in createEvents:
-                # determine function that will be create events
-                evtType = evt['type']
-                createFunc = None
-                if evtType == 'add_outgoing_edge':
-                    createFunc = self.event_manager.CreateEventAddOutputEdge
-                elif evtType == 'add_ingoing_edge':
-                    createFunc = self.event_manager.CreateEventAddInputEdge
-                elif evtType == 'remove_outgoing_edge':
-                    createFunc = self.event_manager.CreateEventRemoveOutputEdge
-                elif evtType == 'remove_ingoing_edge':
-                    createFunc = self.event_manager.CreateEventRemoveInputEdge
-                elif evtType == 'content_change':
-                    createFunc = self.event_manager.CreateEventContentChanged
-                elif evtType == 'delete_element':
-                    createFunc == self.event_manager.CreateEventEraseElement
+    return result
 
-                assert createFunc != None
+  def handleContent(self, ctx, payload):
+    result = []
+    for cmd in payload:
+      t = cmd['command']
+      a = ScAddr(cmd['addr'])
 
-                addr = ScAddr(evt['addr'])
+      if t == 'set':
+        # TODO: support type of content
+        result.append(ctx.SetLinkContent(a, cmd['data']))
+      elif t == 'get':
+        content = ctx.GetLinkContent(a)
+        value = None
+        ctype = None
+        if content:
+          ctype = content.GetType()
 
-                evt_handler = EventHandler()
-                evt = createFunc(addr, evt_handler.OnEmit)
-                evt_handler.Set(evt, self.onEmitEvent)
+          if ctype == ScLinkContent.Int:
+            value = content.AsInt()
+            ctype = 'int'
+          elif ctype == ScLinkContent.Float:
+            value = content.AsFloat()
+            ctype = 'float'
+          elif ctype == ScLinkContent.String:
+            value = content.AsString()
+            ctype = 'string'
 
-                self.events[evt.GetID()] = evt_handler
-                result.append(evt.GetID())
+        result.append({
+            'value': value,
+            'type': ctype
+        })
 
-        return result
+    return result
+
+  def onEmitEvent(self, evt):
+    response = {
+        'id': evt.id,
+        'event': True,
+        'status': True,
+        'payload': [evt.addr.ToInt(), evt.edge_addr.ToInt(), evt.other_addr.ToInt()]
+    }
+    data = json.dumps(response)
+    tornado.ioloop.IOLoop.instance().add_callback(self.sendMessage, data)
+
+  def handleEvents(self, ctx, payload):
+    result = []
+    createEvents = None
+    try:
+      createEvents = payload['create']
+    except KeyError:
+      pass
+
+    if createEvents:
+      for evt in createEvents:
+        # determine function that will be create events
+        evtType = evt['type']
+        createFunc = None
+        if evtType == 'add_outgoing_edge':
+          createFunc = self.event_manager.CreateEventAddOutputEdge
+        elif evtType == 'add_ingoing_edge':
+          createFunc = self.event_manager.CreateEventAddInputEdge
+        elif evtType == 'remove_outgoing_edge':
+          createFunc = self.event_manager.CreateEventRemoveOutputEdge
+        elif evtType == 'remove_ingoing_edge':
+          createFunc = self.event_manager.CreateEventRemoveInputEdge
+        elif evtType == 'content_change':
+          createFunc = self.event_manager.CreateEventContentChanged
+        elif evtType == 'delete_element':
+          createFunc == self.event_manager.CreateEventEraseElement
+
+        assert createFunc != None
+
+        addr = ScAddr(evt['addr'])
+
+        evt_handler = EventHandler()
+        evt = createFunc(addr, evt_handler.OnEmit)
+        evt_handler.Set(evt, self.onEmitEvent)
+
+        self.events[evt.GetID()] = evt_handler
+        result.append(evt.GetID())
+
+    return result
