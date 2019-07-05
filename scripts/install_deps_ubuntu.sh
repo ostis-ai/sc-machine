6f8f5afc05e3f676ed4462927281ed09e8f015a2
Denis Koronchik <denis.koronchik@gmail.com>
2019-07-05 22:47:10 +0300

    Fix some scripts

--- scripts/install_deps_ubuntu.sh
+++ scripts/install_deps_ubuntu.sh
@@ -1,1 +1,5 @@
-sudo apt-get install libglib2.0-dev qt4-qmake libqt4-dev libantlr3c-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-regex-dev make cmake antlr antlr3 libhiredis-dev gcc g++ qtbase5-dev llvm libcurl4-openssl-dev libclang-dev libboost-regex-dev libboost-python-dev python3.5-dev python3.5
+sudo apt-get install librocksdb-dev libglib2.0-dev qt4-qmake libqt4-dev libboost-system-dev \
+                     libboost-filesystem-dev libboost-program-options-dev libboost-regex-dev \
+                     make cmake antlr antlr3 libhiredis-dev gcc g++ qtbase5-dev llvm \
+                     libcurl4-openssl-dev libclang-dev libboost-regex-dev libboost-python-dev \
+                     python3.5-dev python3.5
