53d07f1ab8126ecd89a9cbdd2804424b464962e2
MikhailSadovsky <mishachess@mail.ru>
2020-03-25 16:24:21 +0300

    build(common): Update install and build kb scripts

--- scripts/build_kb.sh
+++ scripts/build_kb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=../bin
-../bin/sc-builder -i ../kb -o ../kb.bin -c -s ../bin/config.ini -f -e ../bin/extension
+../bin/sc-builder -i ../kb -o ../kb.bin -c -s ../bin/config.ini -f -e ../bin/extensions
--- scripts/install_deps_ubuntu.sh
+++ scripts/install_deps_ubuntu.sh
@@ -1,4 +1,4 @@
-sudo apt-get install librocksdb-dev libglib2.0-dev qt4-qmake libqt4-dev libboost-system-dev \
+sudo apt-get install -y librocksdb-dev libglib2.0-dev qt4-qmake libqt4-dev libboost-system-dev \
                      libboost-filesystem-dev libboost-program-options-dev \
                      make cmake antlr gcc g++ llvm-7 \
                      libcurl4-openssl-dev libclang-7-dev libboost-python-dev \
