d50a92993eea53bbbca13b5d4ca0201054a47719
MikhailSadovsky <mishachess@mail.ru>
2020-03-31 17:13:30 +0300

    build(libraries): Adding pip3 installation

--- scripts/install_deps_ubuntu.sh
+++ scripts/install_deps_ubuntu.sh
@@ -2,4 +2,4 @@ sudo apt-get install -y librocksdb-dev libglib2.0-dev qt4-qmake libqt4-dev libbo
                      libboost-filesystem-dev libboost-program-options-dev \
                      make cmake antlr gcc g++ llvm-7 \
                      libcurl4-openssl-dev libclang-7-dev libboost-python-dev \
-                     python3.6-dev python3.6
+                     python3.6-dev python3.6 python3-pip
