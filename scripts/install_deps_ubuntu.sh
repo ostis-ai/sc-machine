49d86592842f473c8ab4e8c65937dbf96550527d
Valik Vityaz <valik.vityaz@gmail.com>
2020-08-26 12:09:15 +0300

    Partially fix build fo 20.04 OS version

--- scripts/install_deps_ubuntu.sh
+++ scripts/install_deps_ubuntu.sh
@@ -1,1 +1,1 @@
-sudo apt-get install libglib2.0-dev qt4-qmake libqt4-dev libantlr3c-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-regex-dev make cmake antlr antlr3 libhiredis-dev gcc g++ qtbase5-dev llvm libcurl4-openssl-dev libclang-dev libboost-regex-dev
+sudo apt-get install libglib2.0-dev libantlr3c-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-regex-dev make cmake antlr antlr3 libhiredis-dev gcc g++ qtbase5-dev llvm libcurl4-openssl-dev libclang-dev libboost-regex-dev
