24beb73434e1f4f844cc578769c6fd4f413de20a
Denis Koronchik <denis.koronchik@gmail.com>
2020-11-02 12:40:26 +0300

    [repo] Remove unused files

--- bindings/python/make.sh
+++
@@ -1,5 +0,0 @@
-swig -I"../../sc-memory/src" -includeall -python pysc.i
-gcc -c pysc_wrap.c -I"/usr/include/python2.7" -I"../../sc-memory/src" -fPIC
-ld -shared pysc_wrap.o -lsc_memory -L../../bin -o _pysc.so
-cp _pysc.so ../../bin/
-cp pysc.py ../../bin/
