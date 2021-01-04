e5385dd14a5f812dc9680982d77800dd4e5eb3c6
Denis Koronchik <denis.koronchik@gmail.com>
2021-01-04 22:38:27 +0300

    [ci] Migrate to github actions

---
+++ scripts/ci/install-deps.sh
@@ -0,0 +1,18 @@
+packagelist=(
+  antlr
+  cmake
+  g++
+  librocksdb-dev
+  libglib2.0-dev
+  libboost-system-dev
+  libboost-filesystem-dev
+  libboost-program-options-dev
+  libcurl4-openssl-dev
+  libclang-7-dev
+  llvm-7
+  make
+  python3
+  python3-pip
+  python3-setuptools
+)
+sudo apt-get install ${packagelist[@]}
--- scripts/ci/install-tests.sh
+++ scripts/ci/install-tests.sh
@@ -7,10 +7,9 @@ pip3 install --user -r requirements.txt
 mkdir build
 pushd build
 
-export CC=${CCOMPILER} CXX=${CXXCOMPILER}
-cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DSC_AUTO_TEST=ON -DSC_BUILD_TESTS=ON -DSC_KPM_SCP=OFF
+cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DSC_AUTO_TEST=ON -DSC_BUILD_SCTP=OFF -DBUILD_TESTING=ON -DSC_KPM_SCP=OFF
 echo "travis_fold:start:MAKE"
-make -j${JOBS}
+make -j4
 echo "travis_fold:end:MAKE"
 
 popd
