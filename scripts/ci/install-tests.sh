37566d5d82283764d600617347635abedf9ff5fa
Denis Koronchik <denis.koronchik@gmail.com>
2021-01-13 16:25:28 +0300

    Review fixes

--- scripts/ci/install-tests.sh
+++ scripts/ci/install-tests.sh
@@ -8,8 +8,8 @@ mkdir build
 pushd build
 
 cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DSC_AUTO_TEST=ON -DSC_BUILD_SCTP=OFF -DBUILD_TESTING=ON -DSC_KPM_SCP=OFF
-echo "travis_fold:start:MAKE"
 make -j4
-echo "travis_fold:end:MAKE"
+
+../bin/sc-builder -i ../tools/builder/tests/kb -o ../bin/sc-builder-test-repo -c -f
 
 popd
--- scripts/ci/run-tests.sh
+++ scripts/ci/run-tests.sh
@@ -2,23 +2,7 @@
 
 set -eo pipefail
 
-pushd bin
-
-echo "travis_fold:start:TEST_SCS"
-./test_scs
-echo "travis_fold:end:TEST_SCS"
-
-echo "travis_fold:start:TEST_MEMORY"
-./sc-memory-tests
-echo "travis_fold:end:TEST_MEMORY"
+pushd build
+ctest -C Debug -V
 
 popd
-
-echo "travis_fold:start:TEST_BUILDER"
-pushd scripts/tests
-./builder_test_kb.sh
-popd
-pushd bin
-./sc-builder-test
-popd
-echo "travis_fold:end:TEST_BUILDER"
