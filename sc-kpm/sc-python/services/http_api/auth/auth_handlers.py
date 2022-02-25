b216eb5237be0e991685996cf702aa579e6e9599
Aliaksandr.Kroshchanka <kroschenko@gmail.com>
2022-02-25 09:22:06 +0300

    [sc-kpm][http_api] Add constants

--- sc-kpm/sc-python/services/http_api/auth/auth_handlers.py
+++ sc-kpm/sc-python/services/http_api/auth/auth_handlers.py
@@ -1,4 +1,9 @@
+import json
+import jwt
+import time
 import tornado
+
+import constants as cnt
 
 
 class PrintTestHandler(tornado.web.RequestHandler):
@@ -9,27 +14,26 @@ class PrintTestHandler(tornado.web.RequestHandler):
 class Token(tornado.web.RequestHandler):
     def post(self):
         database = db.DataBase()
-        username = self.get_argument('user', False)
-        pass_hash = self.get_argument('pass', False)
+        username = self.get_argument(cnt.USER, False)
+        pass_hash = self.get_argument(cnt.PASS, False)
         if database.is_user_valid(username, pass_hash):
-            print("123!")
             access_token_data = Token._generate_access_token()
             response = json.dumps({
-                "access_token": access_token_data.decode(),
-                "token_type": "JWT",
-                "expires_in": tcns.JWT_LIFE_SPAN
+                cnt.ACCESS_TOKEN: access_token_data.decode(),
+                cnt.TOKEN_TYPE: cnt.JWT,
+                cnt.EXPIRES_IN: cnt.JWT_LIFE_SPAN
             })
             self.write(response)
         else:
-            raise tornado.web.HTTPError(403, 'Access denied')
+            raise tornado.web.HTTPError(403, cnt.MSG_ACCESS_DENIED)
 
     @staticmethod
     def _generate_access_token():
         with open('private.pem', 'rb') as file:
             private_key = file.read()
         payload = {
-            "iss": tcns.ISSUER,
-            "exp": time.time() + tcns.JWT_LIFE_SPAN,
+            "iss": cnt.ISSUER,
+            "exp": time.time() + cnt.JWT_LIFE_SPAN,
         }
         access_token = jwt.encode(payload, key=private_key, algorithm='HS256')
         return access_token
@@ -43,6 +47,6 @@ class AddUser(tornado.web.RequestHandler):
         pass_hash = self.get_argument('pass', False)
         email = self.get_argument('email', False)
         if not (username and pass_hash and email):
-            raise tornado.web.HTTPError(400, 'Invalid request')
+            raise tornado.web.HTTPError(400, cnt.MSG_INVALID_REQUEST)
         else:
             database.add_user(username, pass_hash, email)
---
+++ sc-kpm/sc-python/services/http_api/auth/constants.py
@@ -0,0 +1,12 @@
+ACCESS_TOKEN = 'access_token'
+JWT_LIFE_SPAN = 1800
+ISSUER = 'sc-auth-server'
+USER = 'user'
+PASS = 'pass'
+EXPIRES_IN = 'expires_in'
+TOKEN_TYPE = 'token_type'
+JWT = 'JWT'
+
+# messages
+MSG_ACCESS_DENIED = 'Access denied'
+MSG_INVALID_REQUEST = 'Invalid request'
