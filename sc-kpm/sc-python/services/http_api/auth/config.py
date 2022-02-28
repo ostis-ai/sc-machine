43bd85e0c92c4dc45699ba28f5f12c682ba06fc9
Aliaksandr.Kroshchanka <kroschenko@gmail.com>
2022-02-28 18:22:18 +0300

    [http_api] Add new table Role, change add user handler

--- sc-kpm/sc-python/services/http_api/auth/config.py
+++ sc-kpm/sc-python/services/http_api/auth/config.py
@@ -6,7 +6,7 @@ params = {
     cnt.SQLITE_DB_PATH: 'sqlite:///' + '../sc-machine/sc-kpm/sc-python/services/http_api/auth/data.db',
     cnt.PRIVATE_KEY: '../sc-machine/sc-kpm/sc-python/services/http_api/auth/private.pem',
     cnt.PUBLIC_KEY: '../sc-machine/sc-kpm/sc-python/services/http_api/auth/public.pem',
-    # validator params
+    # validator patters
     cnt.USERNAME_PATTERN: '^[a-zA-Z][a-zA-Z0-9-_.]{1,20}$',
     cnt.PASSWORD_PATTERN: '^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\\s).*$',
     # token params
@@ -20,7 +20,14 @@ params = {
         cnt.MSG_ALL_DONE: 0,
         cnt.MSG_INVALID_USERNAME: 1,
         cnt.MSG_INVALID_PASSWORD: 2,
-        cnt.MSG_USER_NOT_FOUND: 3,
-        cnt.MSG_USER_IS_IN_BASE: 4,
+        cnt.MSG_INVALID_ROLE: 3,
+        cnt.MSG_USER_NOT_FOUND: 4,
+        cnt.MSG_USER_IS_IN_BASE: 5,
+    },
+    # roles
+    cnt.ROLES: {
+        cnt.ASSISTANT_ROLE: 0,
+        cnt.PATIENT_ROLE: 1,
+        cnt.ADMIN_ROLE: 256,
     }
 }
--- sc-kpm/sc-python/services/http_api/auth/database.py
+++ sc-kpm/sc-python/services/http_api/auth/database.py
@@ -1,8 +1,10 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.engine import Engine
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, event
+from sqlalchemy.orm import sessionmaker, relationship
 
 import http_api.auth.constants as cnt
 from http_api.auth.config import params
@@ -10,11 +12,26 @@ from http_api.auth.config import params
 Base = declarative_base()
 
 
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
+
 class User(Base):
     __tablename__ = cnt.USER
     id = Column(Integer, primary_key=True, unique=True)
     name = Column(String(255), nullable=False)
     pass_hash = Column(String(255), nullable=False)
+    role_id = Column(Integer, ForeignKey('role.id'))
+    role = relationship("Role")
+
+
+class Role(Base):
+    __tablename__ = cnt.ROLE
+    id = Column(Integer, primary_key=True, unique=True)
+    name = Column(String(255), nullable=False)
 
 
 class DataBase:
@@ -24,6 +41,9 @@ class DataBase:
 
     def init(self) -> None:
         Base.metadata.create_all(self.engine)
+        for key, value in params[cnt.ROLES].items():
+            role = Role(id=value, name=key)
+            self._session().merge(role)
         self._session().commit()
 
     def _session(self):
@@ -43,8 +63,11 @@ class DataBase:
         self._session().merge(u)
         self._session().commit()
 
-    def add_user(self, name, pass_hash):
-        new_user = User(name=str(name),
-                        pass_hash=str(pass_hash))
-        self._session().add(new_user)
-        self._session().commit()
+    def add_user(self, name, pass_hash, role_id):
+        new_user = User(name=str(name), pass_hash=str(pass_hash), role_id=int(role_id))
+        try:
+            self._session().add(new_user)
+            self._session().commit()
+        except IntegrityError:
+            return False
+        return True
