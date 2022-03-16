412eebbc829c96e318b14e498f9c39f252856c50
Viktor Markovec <markvik42@gmail.com>
2022-03-16 18:25:31 +0300

    [ci] Refactor ci for ostis-ai

--- scripts/ci/make-tests.sh
+++ scripts/ci/make-tests.sh
@@ -7,7 +7,7 @@ pip3 install --user -r requirements.txt
 mkdir build
 pushd build
 
-cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DSC_AUTO_TEST=ON -DSC_BUILD_SCTP=OFF -DBUILD_TESTING=ON -DSC_KPM_SCP=OFF
+cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DSC_AUTO_TEST=ON -DSC_BUILD_SCTP=OFF -DSC_BUILD_TESTS=ON -DSC_KPM_SCP=OFF
 make -j4
 
 ../bin/sc-builder -i ../tools/builder/tests/kb -o ../bin/sc-builder-test-repo -c -f
