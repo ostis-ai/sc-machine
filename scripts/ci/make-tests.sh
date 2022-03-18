c3b70f4e0d4e98b4f1288619b2c3810b9f1da075
Viktor Markovec <markvik42@gmail.com>
2022-03-18 09:58:35 +0300

    Review fixes

--- scripts/ci/make-tests.sh
+++ scripts/ci/make-tests.sh
@@ -7,13 +7,11 @@ pip3 install --user -r requirements.txt
 mkdir build
 pushd build
 
-cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DSC_AUTO_TEST=ON -DSC_BUILD_SCTP=OFF -DSC_BUILD_TESTS=ON -DSC_KPM_SCP=OFF
+cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DSC_AUTO_TEST=ON -DSC_BUILD_TESTS=ON -DSC_KPM_SCP=OFF
+echo ::group::Make
 make -j4
+echo ::endgroup::
 
 ../bin/sc-builder -i ../tools/builder/tests/kb -o ../bin/sc-builder-test-repo -c -f
-
-echo "travis_fold:start:builder_kb"
-../bin/sc-builder -i ../tools/builder/tests/kb -o ../bin/sc-builder-test-repo -c -f
-echo "travis_fold:end:builder_kb"
 
 popd
