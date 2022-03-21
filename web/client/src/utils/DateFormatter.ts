5a6e0422e6cec92ac90376a7c05c35cf8b04298b
FallenChromium <fallenchromium@gmail.com>
2022-03-21 16:53:36 +0300

    [tools] Remove web client

--- web/client/src/utils/DateFormatter.ts
+++
@@ -1,4 +0,0 @@
-export function DateToText(date: Date): string {
-  // TODO: add today, yesterday prefixes
-  return date.toLocaleTimeString();
-}
--- web/client/src/utils/server/index.ts
+++
@@ -1,3 +0,0 @@
-export { ServerRoot } from './ServerRoot';
-export { ServerKeynodes } from './ServerKeynodes';
-export { ServerTemplates } from './ServerTemplates';
