cc83511a3244e6e8b00db4f780a80e515a617989
NikitaZotov <nikita.zotov.belarus@gmail.com>
2022-06-16 15:19:08 +0300

    [test][sc-server] Implement sc-client, add create, check and delete elements tests

--- sc-tools/sc-server/tests/tests.cmake
+++ sc-tools/sc-server/tests/tests.cmake
@@ -6,3 +6,7 @@ make_tests_from_folder(${CMAKE_CURRENT_LIST_DIR}/units
   NAME sc-server-tests
   DEPENDS sc-memory sc-server-lib
   INCLUDES ${SC_MEMORY_SRC} ${GLIB2_LIBRARIES})
+
+if (${SC_CLANG_FORMAT_CODE})
+	target_clangformat_setup(sc-server-tests)
+endif ()
--- sc-tools/sc-server/tests/units/test_connection.cpp
+++ sc-tools/sc-server/tests/units/test_connection.cpp
@@ -3,8 +3,12 @@
 #include "sc-memory/sc_link.hpp"
 
 #include "sc_server_test.hpp"
+#include "../sc_client.hpp"
 
 TEST_F(ScServerTest, Connection)
 {
-
+  ScClient client;
+  EXPECT_TRUE(client.Connect(m_server->GetUri()));
+  client.Run();
+  client.Stop();
 }
