4d40aa9d2ccae30ea78906d53ab04d6eed2900ab
NikitaZotov <nikita.zotov.belarus@gmail.com>
2022-07-06 14:25:27 +0300

    [tests][sc-server] Add n connections test for 100 clients and 1000 clients

--- sc-tools/sc-server/tests/units/test_connection.cpp
+++ sc-tools/sc-server/tests/units/test_connection.cpp
@@ -13,28 +13,54 @@ TEST_F(ScServerTest, Connection)
   client.Stop();
 }
 
-// TEST_F(ScServerTest, OneHundredConnections)
-//{
-//  size_t const connections = 100;
-//  std::queue<std::unique_ptr<ScClient>> clients;
-//
-//  size_t i = 0;
-//  while (i++ < connections)
-//  {
-//    auto client = std::make_unique<ScClient>();
-//    EXPECT_TRUE(client->Connect(m_server->GetUri()));
-//    client->Run();
-//    clients.push(std::move(client));
-//  }
-//
-//  sleep(5);
-//
-//  i = 0;
-//  while (clients.empty() == SC_FALSE)
-//  {
-//    auto & client = clients.front();
-//    client->Stop();
-//    clients.pop();
-//    ++i;
-//  }
-//}
+void TEST_N_CONNECTIONS(std::unique_ptr<ScServer> const & server, size_t const amount)
+{
+  size_t const CONNECTIONS = amount;
+  std::queue<std::unique_ptr<ScClient>> clients;
+
+  auto start = std::chrono::high_resolution_clock::now();
+
+  size_t const WAIT_BETWEEN_CLIENTS_CONNECT = 10;
+  size_t i = 0;
+  while (i++ < CONNECTIONS)
+  {
+    auto client = std::make_unique<ScClient>();
+    EXPECT_TRUE(client->Connect(server->GetUri()));
+    client->Run();
+    clients.push(std::move(client));
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(WAIT_BETWEEN_CLIENTS_CONNECT));
+  }
+
+  size_t const WAIT_BETWEEN_CONNECT_DISCONNECT = 5000;
+  std::this_thread::sleep_for(std::chrono::milliseconds(WAIT_BETWEEN_CONNECT_DISCONNECT));
+
+  i = 0;
+  while (clients.empty() == SC_FALSE)
+  {
+    auto & client = clients.front();
+    client->Stop();
+    clients.pop();
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(WAIT_BETWEEN_CLIENTS_CONNECT));
+    ++i;
+  }
+
+  auto end = std::chrono::high_resolution_clock::now();
+
+  std::chrono::duration<double, std::milli> elapsed = end - start;
+  std::cout << "Elapsed "
+            << elapsed.count() - WAIT_BETWEEN_CONNECT_DISCONNECT -
+                   2. * (double)CONNECTIONS * WAIT_BETWEEN_CLIENTS_CONNECT
+            << " ms\n";
+}
+
+TEST_F(ScServerTest, OneHundredConnections)  // 206.55 ms
+{
+  TEST_N_CONNECTIONS(m_server, 100);
+}
+
+TEST_F(ScServerTest, OneThousandConnections)  // 2583.39 ms
+{
+  TEST_N_CONNECTIONS(m_server, 1000);
+}
