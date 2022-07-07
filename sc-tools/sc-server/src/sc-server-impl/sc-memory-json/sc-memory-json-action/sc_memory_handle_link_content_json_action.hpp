d72b76d46ad6f1c8396f205b2b2ec37ddd041821
NikitaZotov <nikita.zotov.belarus@gmail.com>
2022-07-07 13:45:10 +0300

    [tests][sc-server] Add types for link contents

--- sc-tools/sc-server/src/sc-server-impl/sc-memory-json/sc-memory-json-action/sc_memory_handle_link_content_json_action.hpp
+++ sc-tools/sc-server/src/sc-server-impl/sc-memory-json/sc-memory-json-action/sc_memory_handle_link_content_json_action.hpp
@@ -16,34 +16,70 @@ public:
       std::string const & type = atom["command"];
 
       if (type == "set")
-      {
-        ScAddr const & linkAddr = ScAddr(atom["addr"].get<size_t>());
-        std::string const & contentType = atom["type"];
-        std::string const & data = atom["data"];
-
-        ScLink link{*context, linkAddr};
-        responsePayload.push_back(link.Set(data));
-      }
+        responsePayload.push_back(SetContent(context, atom));
       else if (type == "get")
-      {
-        ScAddr const & linkAddr = ScAddr(atom["addr"].get<size_t>());
-        ScLink link{*context, linkAddr};
-
-        responsePayload.push_back({{"value", link.Get<std::string>()}, {"type", "string"}});
-      }
+        responsePayload.push_back(GetContent(context, atom));
       else if (type == "find")
-      {
-        std::string const & data = atom["data"];
-
-        ScAddrVector const & vector = context->FindLinksByContent(data);
-        std::vector<size_t> hashes;
-        for (auto const & addr : vector)
-          hashes.push_back(addr.Hash());
-
-        responsePayload.push_back(hashes);
-      }
+        responsePayload.push_back(FindLinksByContent(context, atom));
     }
 
     return responsePayload;
+  }
+
+private:
+  sc_bool SetContent(ScMemoryContext * context, ScMemoryJsonPayload const & atom)
+  {
+    ScAddr const & linkAddr = ScAddr(atom["addr"].get<size_t>());
+    std::string const & contentType = atom["type"];
+    auto const & data = atom["data"];
+
+    ScLink link{*context, linkAddr};
+
+    if (contentType == "string" || contentType == "binary")
+      return link.Set(data.get<std::string>());
+    else if (contentType == "int")
+      return link.Set(data.get<sc_int>());
+    else if (contentType == "float")
+      return link.Set(data.get<float>());
+
+    return SC_FALSE;
+  }
+
+  ScMemoryJsonPayload GetContent(ScMemoryContext * context, ScMemoryJsonPayload const & atom)
+  {
+    ScAddr const & linkAddr = ScAddr(atom["addr"].get<size_t>());
+    ScLink link{*context, linkAddr};
+
+    ScMemoryJsonPayload answer;
+    if (link.IsType<std::string>() || link.DetermineType() == ScLink::Type::Unknown)
+      return {{"value", link.GetAsString()}, {"type", "string"}};
+    else if (link.DetermineType() >= ScLink::Type::Int8 && link.DetermineType() <= ScLink::Type::UInt64)
+      return {{"value", link.Get<sc_int>()}, {"type", "int"}};
+    else if (link.IsType<double>() || link.IsType<float>())
+      return {{"value", link.Get<float>()}, {"type", "float"}};
+
+    return {};
+  }
+
+  std::vector<size_t> FindLinksByContent(ScMemoryContext * context, ScMemoryJsonPayload const & atom)
+  {
+    auto const & data = atom["data"];
+    ScAddrVector vector;
+    if (data.is_string())
+      vector = context->FindLinksByContent(data.get<std::string>());
+    else if (data.is_number_integer())
+      vector = context->FindLinksByContent(std::to_string(data.get<sc_int>()));
+    else if (data.is_number_float())
+    {
+      std::stringstream stream;
+      stream << data.get<float>();
+      vector = context->FindLinksByContent(stream.str());
+    }
+
+    std::vector<size_t> hashes;
+    for (auto const & addr : vector)
+      hashes.push_back(addr.Hash());
+
+    return hashes;
   }
 };
