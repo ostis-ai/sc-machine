57ac1ca00d963cea698c217debf4182d6718ff7b
FallenChromium <fallenchromium@gmail.com>
2022-07-28 21:32:25 +0300

    [cmake] Fix make_all.sh cmake target folder

--- scripts/make_all.sh
+++ scripts/make_all.sh
@@ -1,3 +1,3 @@
 APP_ROOT_PATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd)
-cmake -B ${APP_ROOT_PATH}/build $@
+cmake -B ${APP_ROOT_PATH}/build ${APP_ROOT_PATH} $@
 cmake --build ${APP_ROOT_PATH}/build -j$(nproc)
