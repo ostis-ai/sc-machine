3eb9e05e1237509963747aef487d9d6883c3b902
FallenChromium <fallenchromium@gmail.com>
2022-08-19 17:37:45 +0300

    [tools][sc-builder] Remove extensions flag from the script

--- scripts/build_kb.py
+++ scripts/build_kb.py
@@ -112,8 +112,7 @@ def prepare_kb(kb_to_prepare: str, logfile: str):
 def build_kb(kb_bin_folder: str, kb_to_build: str, ostis_path: str):
     os.makedirs(kb_bin_folder, exist_ok=True)
     # call sc-builder with required parameters and return the exitcode of the command
-    return os.system(" ".join([join(ostis_path, "sc-builder"), "-f", "--clear", "-i", kb_to_build, "-o", kb_bin_folder,
-                               "-e", join(ostis_path, "extensions")]))
+    return os.system(" ".join([join(ostis_path, "sc-builder"), "-f", "--clear", "-i", kb_to_build, "-o", kb_bin_folder]))
 
 
 def main(args: dict):
