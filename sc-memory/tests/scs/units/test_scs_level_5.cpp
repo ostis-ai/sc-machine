5df6f1b3425d88b1b78db812f5d3d51c68d36d47
NikitaZotov <nikita.zotov.belarus@gmail.com>
2022-08-26 16:04:15 +0300

    [tests][scs] Add test for attribute statements

--- sc-memory/tests/scs/units/test_scs_level_5.cpp
+++ sc-memory/tests/scs/units/test_scs_level_5.cpp
@@ -45,3 +45,54 @@ TEST(scs_level_5, simple)
 
   EXPECT_EQ(triples[3].m_target, triples[2].m_edge);
 }
+
+TEST(scs_level_5, attrs)
+{
+  std::string const data =
+      "set1 => attr1: item1;;"
+      "set2 => attr2:: item2;;"
+      "set3 => attr3|: item3;;";
+
+  scs::Parser parser;
+
+  EXPECT_TRUE(parser.Parse(data));
+
+  TripleTester tester(parser);
+  tester({
+      {
+          { ScType::NodeConst, "set1" },
+          { ScType::EdgeDCommonConst, scs::Visibility::Local },
+          { ScType::NodeConst, "item1" }
+      },
+      {
+          { ScType::NodeConst, "attr1" },
+          { ScType::EdgeAccessConstPosPerm, scs::Visibility::Local },
+          { ScType::EdgeDCommonConst, scs::Visibility::Local }
+      },
+      {
+          { ScType::NodeConst, "set2" },
+          { ScType::EdgeDCommonConst, scs::Visibility::Local },
+          { ScType::NodeConst, "item2" }
+      },
+      {
+          { ScType::NodeConst, "attr2" },
+          { ScType::EdgeAccessVarPosPerm, scs::Visibility::Local },
+          { ScType::EdgeDCommonConst, scs::Visibility::Local }
+      },
+      {
+          { ScType::NodeConst, "set3" },
+          { ScType::EdgeDCommonConst, scs::Visibility::Local },
+          { ScType::NodeConst, "item3" }
+      },
+      {
+          { ScType::NodeConst, "attr3" },
+          { ScType::EdgeAccessConstNegPerm, scs::Visibility::Local },
+          { ScType::EdgeDCommonConst, scs::Visibility::Local }
+      }
+  });
+
+  auto const & triples = parser.GetParsedTriples();
+  EXPECT_EQ(triples.size(), 6u);
+
+  EXPECT_EQ(triples[3].m_target, triples[2].m_edge);
+}
