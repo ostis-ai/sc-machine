0b9e9edad510d0dd770fbf30e22a61c6e3039599
FallenChromium <fallenchromium@gmail.com>
2022-08-29 17:58:57 +0300

    [docker][scripts] Delete run_sc_server, introduce docker entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM debian:11.4-slim as base
+FROM ubuntu:focal as base
 
 ENV CCACHE_DIR=/ccache
 USER root
 
-#install dev dependencies
+#install runtime dependencies
 COPY scripts/install_deps_ubuntu.sh /tmp/sc-machine/scripts/install_deps_ubuntu.sh
 COPY requirements.txt /tmp/sc-machine/requirements.txt
 RUN apt update && apt install -y --no-install-recommends sudo && /tmp/sc-machine/scripts/install_deps_ubuntu.sh
@@ -11,13 +11,13 @@ RUN apt update && apt install -y --no-install-recommends sudo && /tmp/sc-machine
 #build using ccache
 FROM base as devdeps
 RUN /tmp/sc-machine/scripts/install_deps_ubuntu.sh --dev
-WORKDIR /sc-machine
 
 FROM devdeps as devcontainer
-RUN apt install -y --no-install-recommends git cppcheck valgrind gdb bash-completion ninja-build
+RUN apt install -y --no-install-recommends git cppcheck valgrind gdb bash-completion ninja-build curl
 ENTRYPOINT ["/bin/bash"]
 
 FROM devdeps as builder
+WORKDIR /sc-machine
 COPY . .
 RUN --mount=type=cache,target=/ccache/ ./scripts/make_all.sh
 
@@ -31,4 +31,4 @@ WORKDIR /sc-machine/scripts
 
 EXPOSE 8090
 
-ENTRYPOINT ["/bin/sh", "-c", "python3 build_kb.py /kb -c /sc-machine/config/sc-machine.ini && python3 run_sc_server.py -c /sc-machine/config/sc-machine.ini"]
+ENTRYPOINT ["/bin/bash", "/sc-machine/scripts/docker_entrypoint.sh"]
---
+++ scripts/docker_entrypoint.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+set -e # stop script execution if any errors are encountered
+
+# the path to the script directory. Used for relative paths
+SCRIPTS_PATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
+
+# script help info
+function usage() {
+    cat <<USAGE
+
+    Usage: $0 [--rebuild_kb <PATH>] [sc-server args]
+
+    Options:
+        build <PATH>:       rebuilds KB from sources (provide absolute path to the source folder or repo.path file)
+        serve <args>:       Starts sc-server. Arguments passed to this command will be redirected to sc-server binary. If no arguments were given, uses "-h 0.0.0.0 -c /sc-machine/config/sc-machine.ini" as default sc-server arguments. Add these settings yourself if you are planning to use custom arguments.
+
+        Setting REBUILD_KB evironment variable inside the container will trigger a KB rebuild. Setting custom starting point for build_kb.py can be done using KB_PATH environment variable, "/kb" is used as a default KB_PATH.
+
+USAGE
+    "$SCRIPTS_PATH"/../bin/sc-server --help
+    exit 1
+}
+
+function rebuild_kb() {
+    if [ -e "$1" ]; then
+        python3 "$SCRIPTS_PATH/build_kb.py" -c "$SCRIPTS_PATH/../config/sc-machine.ini" "$1"
+    elif [ -n "$KB_PATH" ]; then
+        echo "$KB_PATH is set as a KB path by the environment variable"
+        python3 "$SCRIPTS_PATH/build_kb.py" -c "$SCRIPTS_PATH/../config/sc-machine.ini" "$KB_PATH" 
+    else
+        echo "Invalid KB source path provided."
+        exit 1
+    fi
+}
+
+function start_server() {
+if [ "$REBUILD_KB" -eq 1 ]; then
+    # this expands to $KB_PATH if it's non-null and expands to "/kb" otherwise.
+    rebuild_kb "${KB_PATH:-"/kb"}"
+fi
+
+# if arguments were provided, use them instead of the default ones.
+if [ -e "$1" ]; then
+    # you should provide the config file path and host settings yourself in case you want to use custom options!
+    "$SCRIPTS_PATH"/../bin/sc-server "$@"
+else
+    echo "Using default arguments."
+    "$SCRIPTS_PATH"/../bin/sc-server -c "$SCRIPTS_PATH"/../config/sc-machine.ini -h 0.0.0.0
+fi
+}
+
+
+# parse script commands
+case $1 in
+
+    # rebuild KB in case the build command was passed
+    build)
+        rebuild_kb "$2"
+        ;;
+
+    #launch sc-server
+    serve)
+        start_server "${@:2}"
+        ;;
+
+    # show help
+    --help)
+        usage
+        ;;
+    help)
+        usage
+        ;;
+    -h)
+        usage 
+        ;;
+
+    # All invalid commands will invoke usage page
+    *)
+        usage
+        ;;
+esac
