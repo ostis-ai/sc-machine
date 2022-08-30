20c15c7d7a10d56782bf81dff7a234193a53fa55
FallenChromium <fallenchromium@gmail.com>
2022-08-30 14:34:33 +0300

    [scripts] Quote paths in make_all.sh

--- scripts/make_all.sh
+++ scripts/make_all.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 APP_ROOT_PATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd)
-cmake -B ${APP_ROOT_PATH}/build ${APP_ROOT_PATH} $@
-cmake --build ${APP_ROOT_PATH}/build -j$(nproc)
+cmake -B "${APP_ROOT_PATH}"/build "${APP_ROOT_PATH}" "$@"
+cmake --build "${APP_ROOT_PATH}"/build -j$(nproc)
