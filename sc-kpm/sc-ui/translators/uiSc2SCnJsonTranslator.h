0704b123f4c8d51f95061414078403a3d201e207
Viktor Markovec <markvik42@gmail.com>
2023-01-20 11:46:41 +0300

    [kpm][ui] Change scn json translator for new scn representation

--- sc-kpm/sc-ui/translators/uiSc2SCnJsonTranslator.h
+++ sc-kpm/sc-ui/translators/uiSc2SCnJsonTranslator.h
@@ -8,19 +8,11 @@
 #define _uiSc2SCnJsonTranslator_h_
 
 #include "uiTranslatorFromSc.h"
+#include "uiTranslators.h"
 
 struct sScElementInfo
 {
   typedef std::list<sScElementInfo *> tScElementInfoList;
-
-  typedef enum
-  {
-    VT_NODE,
-    VT_EDGE,
-    VT_SET,
-    VT_CONTOUR
-
-  } eVisualType;
 
   sc_type type;
   sc_addr addr;
@@ -31,68 +23,7 @@ struct sScElementInfo
   tScElementInfoList outputArcs;
   tScElementInfoList inputArcs;
 
-  eVisualType visualType;
-  bool isInSentenceTree;
-};
-
-class uiSCnSentenceNode
-{
-  typedef enum
-  {
-    ST_KEYWORD,
-    ST_PREDICATE,
-    ST_NODE
-  } eSentenceNodeType;
-
-public:
-  typedef std::list<uiSCnSentenceNode *> tSentenceNodeList;
-
-  //! Create keyword sentence node
-  explicit uiSCnSentenceNode(sScElementInfo * keywordEl);
-  virtual ~uiSCnSentenceNode();
-
-  //! Build tree based on keyword. This node must be a keyword
-  void buildTree();
-  //! Balance tree
-  void balance();
-  //! Generates json for specified tree
-  const String & json();
-
-  //! Append new child tree node to this one
-  uiSCnSentenceNode * createChildNode(eSentenceNodeType type);
-
-protected:
-  //! Destroy tree recursively
-  void destroyChilds();
-
-  //! Removes node from childs list
-  void _removeChildNode(uiSCnSentenceNode * child);
-  //! Appends new node to childs list
-  void _appendChildNode(uiSCnSentenceNode * child);
-  //! Check if specified tree node exists in child list of this one
-  bool _hasChildNode(uiSCnSentenceNode * child) const;
-
-private:
-  explicit uiSCnSentenceNode();
-
-  //! Create child predicate node
-  void _createChildPredicate(sScElementInfo * arc);
-
-private:
-  //! Pointer to parent sentence tree node
-  uiSCnSentenceNode * mParent;
-  //! Type of sentence tree node
-  eSentenceNodeType mType;
-  //! List of conotur article root nodes
-  tSentenceNodeList mContourSentences;
-  //! List of child set sentences
-  tSentenceNodeList mChildSetSentences;
-  //! List of child sentences
-  tSentenceNodeList mChildSentences;
-  //! Pointer to sc-element info
-  sScElementInfo * mElementInfo;
-  //! String that contains cache of generated json for tree
-  String mJSONData;
+  bool isInTree;
 };
 
 /*!
@@ -111,18 +42,27 @@ protected:
   //! @copydoc uiTranslateFromSc::runImpl
   void runImpl();
 
-  /*! Translate one sc-element semantic neighborhood
-   * @param addr sc-addr of sc-element to translate
-   * @param isKeyword Keyword flag
-   * @return Returns string that json representaion of sc-element
-   */
-  String translateElement(sc_addr addr, bool isKeyword);
-
-  //! Check if specified sc-element included in output construction
-  bool isInOutputConstruction(sc_addr addr) const;
-
   //! Collect information of trsnslated sc-elements and store it
   void collectScElementsInfo();
+
+  // Generate json for specified element
+  sc_json json(sScElementInfo * elInfo, int level);
+
+  // Get childrens for specified element
+  sc_json getChildrens(sScElementInfo * elInfo, bool isFullLinkedNodes, int level);
+
+  // Get childrens by direction for specified arcs list
+  sc_json getChildrensByDirection(sScElementInfo::tScElementInfoList arcs, String direction);
+
+  // Get base json information about specified element
+  sc_json getBaseInfo(sScElementInfo * elInfo);
+
+  // Group childrens by modifiers
+  sc_json groupChildrensByModifier(sc_json childrens);
+
+  sc_json getJsonOfLinkedNodes(sc_json childrens, int level);
+
+  bool getLinkContent(sc_addr link_addr, String & content);
 
 protected:
   //! List of keywords
@@ -132,8 +72,8 @@ protected:
   tScElemetsInfoMap mScElementsInfo;
   //! Pull of sc-elements information (used to prevent many memory allocations)
   sScElementInfo * mScElementsInfoPool;
-  //! List of articles root elements
-  uiSCnSentenceNode::tSentenceNodeList mRootSentences;
+  //max level of full discripted node
+  const int maxLevel = 2;
 };
 
 #endif  // _uiSc2SCnJsonTranslator_h_
