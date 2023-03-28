66e1ffabd62251e47747f7369369b35a37b554da
Valerian <ivashenko@bsuir.by>
2023-03-28 16:21:14 +0300

    [tests][kpm][core] Log agents work

--- sc-memory/sc-core/sc-store/sc_defines.h
+++ sc-memory/sc-core/sc-store/sc_defines.h
@@ -15,18 +15,9 @@
 #  define SC_DEBUG_MODE 0
 #endif
 
-/*! Bound empty slot search
- *
- * Can be used just with USE_SEGMENT_EMPTY_SLOT_BUFFER = 0
- */
-#define BOUND_EMPTY_SLOT_SEARCH 0
-
-//! Enable network scaling
-#define USE_NETWORK_SCALE 0
-
 #define MAX_PATH_LENGTH 1024
 
-#define SC_CONCURRENCY_LEVEL 32   // max number of independent threads that can work in parallel with memory
+#define SC_CONCURRENCY_LEVEL 32 // max number of independent threads that can work in parallel with memory
 #define SC_SEGMENT_CACHE_SIZE 32  // size of segments cache
 
 #if defined(SC_MEMORY_SELF_BUILD)
