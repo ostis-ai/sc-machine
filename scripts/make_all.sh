513312f3b8894346edd1491e4e4183c0d66e37af
SHtress <stas.butrin1331@gmail.com>
2023-04-06 22:23:48 +0300

    [cmake][scripts] Add flag for sc-component-manager support

--- scripts/make_all.sh
+++ scripts/make_all.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 APP_ROOT_PATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd)
-cmake -B "${APP_ROOT_PATH}"/build "${APP_ROOT_PATH}" "$@"
+COMPONENT_MANAGER_SUPPORT=False
+
+while getopts ":m" flag; do
+    case "${flag}" in
+        m)
+            COMPONENT_MANAGER_SUPPORT=True
+            ;;
+        *)
+            return
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+cmake -B "${APP_ROOT_PATH}"/build "${APP_ROOT_PATH}" "$@" "-DSC_COMPONENT_MANAGER:INTERNAL=$COMPONENT_MANAGER_SUPPORT"
 cmake --build "${APP_ROOT_PATH}"/build -j$(nproc)
