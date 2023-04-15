76cbcb036cd8f2e6d7aa7811db0a736d988e3399
MksmOrlov <tawnhall8.5@gmail.com>
2023-04-15 11:40:45 +0300

    [test][kpm] getNextFromSet util

--- sc-kpm/tests/sc-utils/sc-agents-common-utils.cpp
+++ sc-kpm/tests/sc-utils/sc-agents-common-utils.cpp
@@ -7,38 +7,10 @@
 #include "sc-memory/sc_scs_helper.hpp"
 
 #include "sc-agents-common/keynodes/coreKeynodes.hpp"
-#include "sc-agents-common/utils/IteratorUtils.hpp"
 #include "sc-agents-common/utils/GenerationUtils.hpp"
 #include "sc-agents-common/utils/AgentUtils.hpp"
 
 #include "tests/test-agents/FinishActionTestAgent.hpp"
-
-TEST_F(ScMemoryTest, getAnyByOutRelation)
-{
-  std::string const data =
-      "dialog"
-      "  <- concept_dialog;"
-      "  -> rrel_last: message; "
-      "  -> rrel_1: message;; ";
-
-  SCsHelper helper(*m_ctx, std::make_shared<DummyFileInterface>());
-  EXPECT_TRUE(helper.GenerateBySCsText(data));
-
-  ScAddr const dialog = m_ctx->HelperFindBySystemIdtf("dialog");
-  EXPECT_TRUE(dialog.IsValid());
-  ScAddr const relation = m_ctx->HelperFindBySystemIdtf("rrel_1");
-  EXPECT_TRUE(relation.IsValid());
-  ScAddr const relationLast = m_ctx->HelperFindBySystemIdtf("rrel_last");
-  EXPECT_TRUE(relationLast.IsValid());
-
-  ScAddr const message = utils::IteratorUtils::getAnyByOutRelation(&*m_ctx, dialog, relation);
-  EXPECT_TRUE(message.IsValid());
-
-  ScAddr const messageCopy = utils::IteratorUtils::getAnyByOutRelation(&*m_ctx, dialog, relationLast);
-  EXPECT_TRUE(messageCopy.IsValid());
-
-  EXPECT_TRUE(message == messageCopy);
-}
 
 TEST_F(ScMemoryTest, wrapInOrientedSetBySequenceRelation)
 {
