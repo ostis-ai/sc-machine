f0b5c870333d4967b86b3a11f3b7d1757f63225b
FallenChromium <fallenchromium@gmail.com>
2023-04-18 03:27:23 +0300

    [scripts] Delete outdated boost-python dep on macOS

--- scripts/install_deps_macOS.sh
+++ scripts/install_deps_macOS.sh
@@ -1,1 +1,2 @@
-brew install boost glib llvm@14 antlr antlr4-cpp-runtime temurin pkgconfig cmake boost-python3 ccache websocketpp nlohmann-json
+#! /usr/bin/env bash
+brew install boost glib llvm@14 antlr antlr4-cpp-runtime temurin pkgconfig cmake ccache websocketpp nlohmann-json
