ee40ab14717e5cd18714376f3e67273661ec85b4
SHtress <stas.butrin1331@gmail.com>
2023-04-19 20:27:14 +0300

    [scripts] Update scripts for component manager

--- scripts/run_sc_component_manager.sh
+++ scripts/run_sc_component_manager.sh
@@ -3,19 +3,23 @@
 set -e
 
 APP_ROOT_PATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd)
+INTERACTIVE_MODE=""
 
 while getopts 'c:b:' OPTION; do
   case "$OPTION" in
     c)
-      config_path="$OPTARG"
-      CONFIG_ABSOLUTE_PATH=$(cd "$(dirname "$config_path")" && pwd -P)/$(basename "$config_path")
+      CONFIG_PATH="$OPTARG"
+      CONFIG_ABSOLUTE_PATH=$(cd "$(dirname "$CONFIG_PATH")" && pwd -P)/$(basename "$CONFIG_PATH")
       ;;
     b)
-      kb_src_path="$OPTARG"
-      python3 $APP_ROOT_PATH/scripts/build_kb.py $kb_src_path -c $CONFIG_ABSOLUTE_PATH
+      KB_SRC_PATH="$OPTARG"
+      python3 $APP_ROOT_PATH/scripts/build_kb.py $KB_SRC_PATH -c $CONFIG_ABSOLUTE_PATH
+      ;;
+    i)
+      INTERACTIVE_MODE="-i"
       ;;
     h)
-      echo "script usage: $(basename \$0) [-c config_path] [-b kb_src_path]" >&2
+      echo "script usage: $(basename \$0) [-c CONFIG_PATH] [-b KB_SRC_PATH]" >&2
       exit 1
       ;;
   esac
@@ -24,9 +28,9 @@ shift "$(($OPTIND -1))"
 
 if [ -z "$CONFIG_ABSOLUTE_PATH" ]; then
     echo "no config path is provided."
-    echo "script usage: $(basename \$0) [-c config_path] [-b kb_src_path]" >&2
+    echo "script usage: $(basename \$0) [-c CONFIG_PATH] [-b KB_SRC_PATH] [-i]" >&2
     exit 1
 fi
 
-$APP_ROOT_PATH/bin/sc-component-manager -c $CONFIG_ABSOLUTE_PATH -i
+$APP_ROOT_PATH/bin/sc-component-manager -c $CONFIG_ABSOLUTE_PATH $INTERACTIVE_MODE
 
