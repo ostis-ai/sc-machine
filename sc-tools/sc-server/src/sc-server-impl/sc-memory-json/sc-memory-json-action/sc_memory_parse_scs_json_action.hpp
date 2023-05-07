b46acd8a51936b590b8b55dfaed727c319e52240
NikitaZotov <nikita.zotov.belarus@gmail.com>
2023-05-07 11:25:59 +0300

    [tools][server] Add endpoint to parse scs AST

---
+++ sc-tools/sc-server/src/sc-server-impl/sc-memory-json/sc-memory-json-action/sc_memory_parse_scs_json_action.hpp
@@ -0,0 +1,45 @@
+/*
+ * This source file is part of an OSTIS project. For the latest info, see http://ostis.net
+ * Distributed under the MIT License
+ * (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
+ */
+
+#pragma once
+
+#include "sc_memory_json_action.hpp"
+
+#include "sc-memory/scs/scs_parser.hpp"
+
+class ScMemoryParseSCsJsonAction : public ScMemoryJsonAction
+{
+public:
+  ScMemoryParseSCsJsonAction()
+  {
+    m_parser = new scs::Parser();
+  }
+
+  ScMemoryJsonPayload Complete(
+      ScAgentContext * context, ScMemoryJsonPayload requestPayload, ScMemoryJsonPayload & errorsPayload) override
+  {
+    ScMemoryJsonPayload responsePayload = ScMemoryJsonPayload::array();
+
+    for (auto & atom : requestPayload)
+    {
+      std::string const & ast = m_parser->BuildAST(atom.get<std::string>());
+      responsePayload.push_back(ScMemoryJsonPayload::parse(ast));
+    }
+
+    if (responsePayload.is_null())
+      return "{}"_json;
+
+    return responsePayload;
+  }
+
+  ~ScMemoryParseSCsJsonAction() override
+  {
+    delete m_parser;
+  }
+
+private:
+  scs::Parser * m_parser;
+};
