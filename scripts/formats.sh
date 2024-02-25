f1c789e9064a197eae500dfb40204061fff45c49
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-02-25 16:03:10 +0300

    [scripts] Deprecate run_sc_server.sh and implement run_sc_machine.sh

--- scripts/formats.sh
+++ scripts/formats.sh
@@ -2,3 +2,8 @@ stage()
 {
   ([ -n "$(tput setaf 1 2<&-)" ] && { tput setaf 4 && tput bold && echo "$1" && tput sgr0; }) || echo "$1"
 }
+
+warning()
+{
+  ([ -n "$(tput setaf 1 2<&-)" ] && { tput setaf 3 && tput bold && echo "$1" && tput sgr0; }) || echo "$@"
+}
