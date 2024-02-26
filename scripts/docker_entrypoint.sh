7ce39c5427db614a93abf1a1f5851f065e7a04b7
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-02-26 17:39:17 +0300

    [docker] Implement and use sc-machine command

--- scripts/docker_entrypoint.sh
+++ scripts/docker_entrypoint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -eo pipefail # stop script execution if any errors are encountered
 
+CURRENT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
+source "${CURRENT_DIR}/formats.sh"
+
 # script help info
 function usage() {
     cat <<USAGE
@@ -8,10 +11,12 @@ function usage() {
     Usage: 
         $0 build -b binary_path -c config_path [KB path]
         $0 serve -b binary_path -c config_path [sc-server args]
+        $0 run -b binary_path -c config_path [sc-machine args]
 
     Options:
-        build <PATH>:       rebuilds KB from sources (provide absolute path to the source folder or repo.path file)
+        build <PATH>:       Rebuilds KB from sources (provide absolute path to the source folder or repo.path file).
         serve <args>:       Starts sc-server. Arguments passed to this command will be redirected to sc-server binary. If no arguments were given, uses "-h 0.0.0.0" is given as default sc-server arguments. Add these settings yourself if you are planning to use custom arguments.
+        run <args>:         Starts sc-machine. Arguments passed to this command will be redirected to sc-machine binary. You can set sc-server options from common config.
 
         Setting REBUILD_KB environment variable inside the container will trigger a KB rebuild. Setting custom starting point for sc-builder can be done using KB_PATH environment variable, "/kb" is used as a default KB_PATH.
         CONFIG_PATH and BINARY_PATH environment variables can provide the respective settings if the use of flags is undesirable.
@@ -48,6 +53,22 @@ function start_server() {
     fi
 }
 
+function start_machine {
+    if [ -n "$REBUILD_KB" ] && [ "$REBUILD_KB" -eq 1 ]; then
+        # this expands to $KB_PATH if it's non-null and expands to "/kb" otherwise.
+        rebuild_kb "${KB_PATH:-"/kb"}"
+    fi
+
+    # if arguments were provided, use them instead of the default ones.
+    if [ $# -eq 0 ]; then
+        # you should provide the config file path and host settings yourself in case you want to use custom options!
+        echo "Using default arguments."
+        "$BINARY_PATH"/sc-machine -c "$CONFIG_PATH" -e "$BINARY_PATH/extensions"
+    else
+        "$BINARY_PATH"/sc-machine "$@"
+    fi
+}
+
 # parse script commands
 case $1 in
 
@@ -76,7 +97,7 @@ build)
     rebuild_kb "$@"
     ;;
 
-#launch sc-server
+# launch sc-server
 serve)
     shift 1;
     while getopts "b:c:h" opt; do
@@ -97,8 +118,32 @@ serve)
         esac
     done
     shift $((OPTIND - 1))
+    warning "Now sc-server is extension that is loaded by binary \`sc-machine\`. The command \`serve\` is deprecated in sc-machine 0.10.0 and will be removed in sc-machine 0.11.0. Use command \`run\` instead that launch binary \`sc-machine\`."
     start_server "$@"
+    ;;
 
+# launch sc-machine
+run)
+    shift 1;
+    while getopts "b:c:h" opt; do
+        case $opt in
+        b)
+            BINARY_PATH=$OPTARG
+            ;;
+        c)
+            CONFIG_PATH=$OPTARG
+            ;;
+        h)
+            usage
+            ;;
+        \?)
+            echoerr "Invalid option -$OPTARG"
+            usage
+            ;;
+        esac
+    done
+    shift $((OPTIND - 1))
+    start_machine "$@"
     ;;
 
 # show help
