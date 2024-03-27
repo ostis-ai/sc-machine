fea6345d5bc5ab34c6d6957c322e911291f82b2d
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-03-27 15:57:22 +0300

    [scripts] Use common scripts from ostis-scripts

--- scripts/build_sc_machine.sh
+++ scripts/build_sc_machine.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-CURRENT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
-source "${CURRENT_DIR}/formats.sh"
+CURRENT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
+OSTIS_SCRIPTS_DIR="${CURRENT_DIR}/ostis-scripts"
+source "${OSTIS_SCRIPTS_DIR}/message-scripts/messages.sh"
 
 if [[ -z "${ROOT_CMAKE_PATH}" || -z "${CXX_SOURCES_PATH}" || -z "${BUILD_PATH}" || -z "${BINARY_PATH}" ]];
 then
@@ -20,15 +21,15 @@ do
   shift 1
 done
 
-stage "Build sc-machine"
+info "Build sc-machine"
 
 if (( build_force == 1 ));
 then
   rm -rf "${BINARY_PATH}"
 fi
 
-BUILD_SCRIPTS="${CURRENT_DIR}/build-scripts"
+BUILD_SCRIPTS="${OSTIS_SCRIPTS_DIR}/build-scripts"
 "${BUILD_SCRIPTS}/build_cxx_project.sh" -p "${ROOT_CMAKE_PATH}" -s "${CXX_SOURCES_PATH}" -b "${BUILD_PATH}" \
   --cmake-arg "-DSC_BIN_PATH=${BINARY_PATH}" "${ARGS[@]}"
 
-stage "sc-machine is built successfully"
+info "sc-machine is built successfully"
--- scripts/run_sc_server.sh
+++ scripts/run_sc_server.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-CURRENT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
-source "${CURRENT_DIR}/formats.sh"
+CURRENT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
+OSTIS_SCRIPTS_DIR="${CURRENT_DIR}/ostis-scripts"
+source "${OSTIS_SCRIPTS_DIR}/message-scripts/messages.sh"
 
 warning "This script is deprecated in sc-machine 0.10.0 and will be removed in sc-machine 0.11.0. Use script \`run_sc_machine.sh\` instead."
 
