3cab5812396945de058ca3166cdb0fabc8f9c1aa
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-04-30 15:03:11 +0300

    [scripts] Use venv for python dependencies

--- scripts/ci/make_tests.sh
+++ scripts/ci/make_tests.sh
@@ -2,8 +2,6 @@
 
 set -eo pipefail
 
-pip3 install --user -r requirements.txt
-
 cmake -B build -DSC_FILE_MEMORY=${FILE_MEMORY} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DSC_COVERAGE=${COVERAGE} -DSC_AUTO_TEST=ON -DSC_BUILD_TESTS=ON
 echo ::group::Make
 cmake --build build -j$(nproc)
--- scripts/install_deps_python.sh
+++ scripts/install_deps_python.sh
@@ -6,6 +6,7 @@ then
   source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)/set_vars.sh"
 fi
 
+python3 -m venv "${SC_MACHINE_PATH}/.venv"
+source "${SC_MACHINE_PATH}/.venv/bin/activate"
 pip3 install wheel setuptools
 pip3 install -r "${SC_MACHINE_PATH}/requirements.txt"
-
