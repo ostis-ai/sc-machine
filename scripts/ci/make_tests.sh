e1b584a8eb16cd3546866d41b02f6392e77408d3
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-05-19 16:38:34 +0300

    [ci] Remove file memory option from configs

--- scripts/ci/make_tests.sh
+++ scripts/ci/make_tests.sh
@@ -2,9 +2,9 @@
 
 set -eo pipefail
 
-cmake -B build -DSC_FILE_MEMORY=${FILE_MEMORY} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DSC_COVERAGE=${COVERAGE} -DSC_AUTO_TEST=ON -DSC_BUILD_TESTS=ON
+cmake -B build -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" -DSC_COVERAGE="${COVERAGE}" -DSC_AUTO_TEST=ON -DSC_BUILD_TESTS=ON
 echo ::group::Make
-cmake --build build -j$(nproc)
+cmake --build build -j"$(nproc)"
 echo ::endgroup::
 
 pushd build
