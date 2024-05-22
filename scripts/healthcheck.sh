4470e8434dbd4ea341097943784a80420fee530b
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-05-22 15:21:57 +0300

    [scripts][docker] Add script for healthcheck

---
+++ scripts/healthcheck.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+CURRENT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
+source "${CURRENT_DIR}/formats.sh"
+
+if [ -z "${SC_MACHINE_PATH}" ];
+then
+  source "${CURRENT_DIR}/set_vars.sh"
+fi
+
+source "${SC_MACHINE_PATH}/.venv/bin/activate" && python3 "${SC_MACHINE_PATH}/scripts/healthcheck.py"
