91c7230b4a0aeafb7d25bb8ecdfd9be847d6f33e
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-07-01 13:44:17 +0300

    [memory][bench][tests] Fix types for uniform_int_distribution on macOS

--- sc-memory/tests/performance/units/memory_search_link_by_content.hpp
+++ sc-memory/tests/performance/units/memory_search_link_by_content.hpp
@@ -36,7 +36,7 @@ public:
 
       std::random_device rd;
       std::mt19937 gen(rd());
-      std::uniform_int_distribution<char> charDistribution(32, 126);
+      std::uniform_int_distribution<int> charDistribution(32, 126);
 
       for (size_t j = 0; j < stringLength; ++j)
       {
