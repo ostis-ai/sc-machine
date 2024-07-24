9797d76aa332fb0f3f6282b9a6dfb4ccf320b26b
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-07-24 14:58:35 +0300

    [memory][events] Make event types constant for ubuntu-20.04

--- sc-memory/sc-memory/sc_event.cpp
+++ sc-memory/sc-memory/sc_event.cpp
@@ -8,15 +8,15 @@
 
 // Should be equal to C values
 
-ScEvent::Type constexpr ScEvent::Type::Unknown(sc_event_unknown);
-ScEvent::Type constexpr ScEvent::Type::AddInputArc(sc_event_add_input_arc);
-ScEvent::Type constexpr ScEvent::Type::AddOutputArc(sc_event_add_output_arc);
-ScEvent::Type constexpr ScEvent::Type::AddEdge(sc_event_add_edge);
-ScEvent::Type constexpr ScEvent::Type::RemoveInputArc(sc_event_remove_input_arc);
-ScEvent::Type constexpr ScEvent::Type::RemoveOutputArc(sc_event_remove_output_arc);
-ScEvent::Type constexpr ScEvent::Type::RemoveEdge(sc_event_remove_edge);
-ScEvent::Type constexpr ScEvent::Type::EraseElement(sc_event_erase_element);
-ScEvent::Type constexpr ScEvent::Type::ChangeContent(sc_event_change_content);
+ScEvent::Type const ScEvent::Type::Unknown(sc_event_unknown);
+ScEvent::Type const ScEvent::Type::AddInputArc(sc_event_add_input_arc);
+ScEvent::Type const ScEvent::Type::AddOutputArc(sc_event_add_output_arc);
+ScEvent::Type const ScEvent::Type::AddEdge(sc_event_add_edge);
+ScEvent::Type const ScEvent::Type::RemoveInputArc(sc_event_remove_input_arc);
+ScEvent::Type const ScEvent::Type::RemoveOutputArc(sc_event_remove_output_arc);
+ScEvent::Type const ScEvent::Type::RemoveEdge(sc_event_remove_edge);
+ScEvent::Type const ScEvent::Type::EraseElement(sc_event_erase_element);
+ScEvent::Type const ScEvent::Type::ChangeContent(sc_event_change_content);
 
 ScEvent::~ScEvent() = default;
 
