093094696fba03b3d165e1cc28e292262ffd0732
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-09-02 10:27:37 +0300

    [memory][set] Implement union, intersection, subtraction and equality of sets

--- sc-memory/sc-memory/sc_set.cpp
+++ sc-memory/sc-memory/sc_set.cpp
@@ -8,6 +8,57 @@
 #include "sc_memory.hpp"
 #include "sc_template.hpp"
 
+#include "sc_keynodes.hpp"
+
+// Hash function for ScType pairs
+struct ScSet::ScTypeHashFunc
+{
+  std::size_t operator()(std::pair<ScType, ScType> const & p) const
+  {
+    std::size_t hash1 = p.first;
+    std::size_t hash2 = p.second;
+
+    return hash1 ^ hash2 ^ (hash1 << 1) ^ (hash2 << 1);
+  }
+};
+
+// Equality function for ScType pairs
+struct _SC_EXTERN ScSet::ScTypeEqualFunc
+{
+  bool operator()(std::pair<ScType, ScType> const & a, std::pair<ScType, ScType> const & b) const
+  {
+    return (a.first.BitAnd(b.first) == a.first) && (a.second.BitAnd(b.second) == a.second);
+  }
+};
+
+std::unordered_map<std::pair<ScType, ScType>, std::array<ScType, 3>, ScSet::ScTypeHashFunc, ScSet::ScTypeEqualFunc>
+    ScSet::m_setsAccessArcTypesToUnionAndInteractionResultAccessArcTypes = {
+        {{ScType::EdgeAccessConstPos, ScType::EdgeAccessConstPos},
+         {ScType::EdgeAccessConstPos, ScType::EdgeAccessConstPos, ScType::EdgeAccessConstNeg}},
+        {{ScType::EdgeAccessConstPos, ScType::EdgeAccessConstFuz},
+         {ScType::EdgeAccessConstPos, ScType::EdgeAccessConstFuz, ScType::EdgeAccessConstFuz}},
+        {{ScType::EdgeAccessConstFuz, ScType::EdgeAccessConstPos},
+         {ScType::EdgeAccessConstPos, ScType::EdgeAccessConstFuz, ScType::EdgeAccessConstNeg}},
+        {{ScType::EdgeAccessConstPos, ScType::EdgeAccessConstNeg},
+         {ScType::EdgeAccessConstPos, ScType::EdgeAccessConstNeg, ScType::EdgeAccessConstPos}},
+        {{ScType::EdgeAccessConstNeg, ScType::EdgeAccessConstPos},
+         {ScType::EdgeAccessConstPos, ScType::EdgeAccessConstNeg, ScType::EdgeAccessConstNeg}},
+
+        {{ScType::EdgeAccessConstFuz, ScType::EdgeAccessConstFuz},
+         {ScType::EdgeAccessConstFuz, ScType::EdgeAccessConstFuz, ScType::EdgeAccessConstFuz}},
+        {{ScType::EdgeAccessConstFuz, ScType::EdgeAccessConstNeg},
+         {ScType::EdgeAccessConstFuz, ScType::EdgeAccessConstNeg, ScType::EdgeAccessConstFuz}},
+        {{ScType::EdgeAccessConstNeg, ScType::EdgeAccessConstFuz},
+         {ScType::EdgeAccessConstFuz, ScType::EdgeAccessConstNeg, ScType::EdgeAccessConstNeg}},
+
+        {{ScType::EdgeAccessConstNeg, ScType::EdgeAccessConstNeg},
+         {ScType::EdgeAccessConstNeg, ScType::EdgeAccessConstNeg, ScType::EdgeAccessConstNeg}},
+};
+
+size_t const ScSet::UNITE_SETS_ID = 0;
+size_t const ScSet::INTERSECT_SETS_ID = 1;
+size_t const ScSet::SUBTRACT_SETS_ID = 2;
+
 ScSet::ScSet(ScMemoryContext * context, ScAddr const & setAddr)
   : ScAddr(setAddr)
   , m_context(context)
@@ -16,41 +67,41 @@ ScSet::ScSet(ScMemoryContext * context, ScAddr const & setAddr)
 
 ScSet::~ScSet() = default;
 
-bool ScSet::Append(ScAddr const & elAddr)
+bool ScSet::Append(ScAddr const & elAddr, ScType const & arcType)
 {
-  if (!HasElement(elAddr))
-    return m_context->GenerateConnector(ScType::EdgeAccessConstPosPerm, *this, elAddr).IsValid();
+  if (!Has(elAddr, ScType::EdgeAccessConst))
+    return m_context->GenerateConnector(arcType, *this, elAddr).IsValid();
 
   return false;
 }
 
-bool ScSet::Append(ScAddr const & elAddr, ScAddr const & attrAddr)
+bool ScSet::Append(ScAddr const & elAddr, ScAddr const & attrAddr, ScType const & arcType)
 {
-  if (HasElement(elAddr))
+  if (Has(elAddr, ScType::EdgeAccessConst))
     return false;
 
-  ScAddr const arcAddr = m_context->GenerateConnector(ScType::EdgeAccessConstPosPerm, *this, elAddr);
+  ScAddr const & arcAddr = m_context->GenerateConnector(arcType, *this, elAddr);
   m_context->GenerateConnector(ScType::EdgeAccessConstPosPerm, attrAddr, arcAddr);
 
   return true;
 }
 
-bool ScSet::Remove(ScAddr const & elAddr)
+bool ScSet::Remove(ScAddr const & elAddr, ScType const & arcType)
 {
-  bool found = false;
-  ScIterator3Ptr iter = m_context->CreateIterator3(*this, ScType::EdgeAccessConstPosPerm, elAddr);
-  while (iter->Next())
+  bool isFound = false;
+  ScIterator3Ptr const iterator3 = m_context->CreateIterator3(*this, arcType, elAddr);
+  while (iterator3->Next())
   {
-    m_context->EraseElement(iter->Get(1));
-    found = true;
+    m_context->EraseElement(iterator3->Get(1));
+    isFound = true;
   }
 
-  return found;
+  return isFound;
 }
 
-bool ScSet::HasElement(ScAddr const & elAddr) const
+bool ScSet::Has(ScAddr const & elAddr, ScType const & arcType) const
 {
-  return m_context->CheckConnector(*this, elAddr, ScType::EdgeAccessConstPosPerm);
+  return m_context->CheckConnector(*this, elAddr, arcType);
 }
 
 ScSet & ScSet::operator<<(ScAddr const & elAddr)
@@ -59,11 +110,11 @@ ScSet & ScSet::operator<<(ScAddr const & elAddr)
   return *this;
 }
 
-ScSet & ScSet::operator<<(ScTemplateResultItem const & res)
+ScSet & ScSet::operator<<(ScTemplateResultItem const & searchResultItem)
 {
-  size_t const res_num = res.Size();
-  for (size_t i = 0; i < res_num; ++i)
-    Append(res.m_replacementConstruction[i]);
+  size_t const size = searchResultItem.Size();
+  for (size_t i = 0; i < size; ++i)
+    Append(searchResultItem.m_replacementConstruction[i]);
 
   return *this;
 }
@@ -76,6 +127,366 @@ ScSet & ScSet::operator>>(ScAddr const & elAddr)
 
 bool ScSet::IsEmpty() const
 {
-  ScIterator3Ptr const iter = m_context->CreateIterator3(*this, ScType::EdgeAccessConstPosPerm, ScType::Unknown);
-  return !iter->Next();
+  ScIterator3Ptr const iterator3 = m_context->CreateIterator3(*this, ScType::EdgeAccessPos, ScType::Unknown);
+  return !iterator3->Next();
+}
+
+size_t ScSet::GetPower() const
+{
+  size_t power = 0;
+  ScIterator3Ptr const iterator3 = m_context->Iterator3(*this, ScType::EdgeAccessPos, ScType::Unknown);
+  while (iterator3->Next())
+    power++;
+  return power;
+}
+
+ScSet & ScSet::Unite(ScSet const & otherSet)
+{
+  *this += otherSet;
+  return *this;
+}
+
+ScSet & ScSet::Intersect(ScSet const & otherSet)
+{
+  *this *= otherSet;
+  return *this;
+}
+
+ScSet & ScSet::Subtract(ScSet const & otherSet)
+{
+  *this -= otherSet;
+  return *this;
+}
+
+ScSet const operator+(ScSet const & leftSet, ScSet const & rightSet)
+{
+  ScMemoryContext * context = leftSet.m_context;
+
+  ScAddr const & resultSetAddr =
+      ScSet::SearchBinaryOperationResult(context, leftSet, rightSet, ScKeynodes::nrel_sets_union);
+  if (resultSetAddr.IsValid())
+    return {context, resultSetAddr};
+
+  ScAddrToValueUnorderedMap<ScType> leftSetElements;
+  bool const leftSetIsValidForOperation = leftSet.GetElements(leftSetElements);
+  if (!leftSetIsValidForOperation)
+    SC_THROW_EXCEPTION(
+        utils::ExceptionInvalidParams,
+        "Not able to unite provided sets because left set has outgoing both permanents and temporal sc-arcs.");
+
+  ScAddrToValueUnorderedMap<ScType> rightSetElements;
+  bool const rightSetIsValidForOperation = rightSet.GetElements(rightSetElements);
+  if (!rightSetIsValidForOperation)
+    SC_THROW_EXCEPTION(
+        utils::ExceptionInvalidParams,
+        "Not able to unite provided sets because right set has outgoing both permanents and temporal sc-arcs.");
+
+  ScSet resultSet{context, context->GenerateNode(ScType::NodeConst)};
+  ScAddrUnorderedSet addedElements;
+  auto const & AppendElements = [&](ScAddrToValueUnorderedMap<ScType> const & setElements,
+                                    ScAddrToValueUnorderedMap<ScType> const & otherElements)
+  {
+    for (auto & [elementAddr, arcType] : setElements)
+    {
+      if (addedElements.find(elementAddr) != addedElements.end())
+        continue;
+
+      addedElements.insert(elementAddr);
+
+      ScType resultArcType = arcType;
+      auto const & it = otherElements.find(elementAddr);
+      if (it != otherElements.cend())
+      {
+        ScType const & otherArcType = it->second;
+        resultArcType = ScSet::GetResultAccessArcType(arcType, otherArcType, ScSet::UNITE_SETS_ID)
+                        | ScSet::GetPermanencyAccessArcSubtype(arcType);
+      }
+
+      resultSet.Append(elementAddr, resultArcType);
+    }
+  };
+
+  AppendElements(leftSetElements, rightSetElements);
+  AppendElements(rightSetElements, leftSetElements);
+
+  ScSet::GenerateBinaryOperationResult(context, leftSet, rightSet, ScKeynodes::nrel_sets_union, resultSet);
+
+  return resultSet;
+}
+
+ScSet const operator+=(ScSet const & leftSet, ScSet const & rightSet)
+{
+  return leftSet + rightSet;
+}
+
+ScSet const operator*(ScSet const & leftSet, ScSet const & rightSet)
+{
+  ScMemoryContext * context = leftSet.m_context;
+
+  ScAddr const & resultSetAddr =
+      ScSet::SearchBinaryOperationResult(context, leftSet, rightSet, ScKeynodes::nrel_sets_intersection);
+  if (resultSetAddr.IsValid())
+    return {context, resultSetAddr};
+
+  ScAddrToValueUnorderedMap<ScType> leftSetElements;
+  bool const leftSetIsValidForOperation = leftSet.GetElements(leftSetElements);
+  if (!leftSetIsValidForOperation)
+    SC_THROW_EXCEPTION(
+        utils::ExceptionInvalidParams,
+        "Not able to intersect provided sets because left set has outgoing both permanents and temporal sc-arcs.");
+
+  ScAddrToValueUnorderedMap<ScType> rightSetElements;
+  bool const rightSetIsValidForOperation = rightSet.GetElements(rightSetElements);
+  if (!rightSetIsValidForOperation)
+    SC_THROW_EXCEPTION(
+        utils::ExceptionInvalidParams,
+        "Not able to intersect provided sets because right set has outgoing both permanents and temporal sc-arcs.");
+
+  // Iterate over the smaller set to reduce the number of lookups
+  ScAddrToValueUnorderedMap<ScType> const & smallerSetElements =
+      (leftSetElements.size() < rightSetElements.size()) ? leftSetElements : rightSetElements;
+  ScAddrToValueUnorderedMap<ScType> & largerSetElements =
+      (leftSetElements.size() < rightSetElements.size()) ? rightSetElements : leftSetElements;
+
+  ScSet resultSet{context, context->GenerateNode(ScType::NodeConst)};
+  for (auto & [elementAddr, arcType] : smallerSetElements)
+  {
+    ScType resultArcType = arcType;
+    auto const & it = largerSetElements.find(elementAddr);
+    if (it == largerSetElements.cend())
+      resultArcType = ScType::EdgeAccessConstNegPerm;
+    else
+    {
+      ScType const & otherArcType = it->second;
+      resultArcType = ScSet::GetResultAccessArcType(arcType, otherArcType, ScSet::INTERSECT_SETS_ID)
+                      | ScSet::GetPermanencyAccessArcSubtype(arcType);
+
+      largerSetElements.erase(elementAddr);
+    }
+
+    resultSet.Append(elementAddr, resultArcType);
+  }
+
+  for (auto & [elementAddr, _] : largerSetElements)
+    resultSet.Append(elementAddr, ScType::EdgeAccessConstNegPerm);
+
+  ScSet::GenerateBinaryOperationResult(context, leftSet, rightSet, ScKeynodes::nrel_sets_intersection, resultSet);
+
+  return resultSet;
+}
+
+ScSet const operator*=(ScSet const & leftSet, ScSet const & rightSet)
+{
+  return leftSet * rightSet;
+}
+
+ScSet const operator-(ScSet const & leftSet, ScSet const & rightSet)
+{
+  ScMemoryContext * context = leftSet.m_context;
+
+  ScAddr const & resultSetAddr =
+      ScSet::SearchBinaryOperationResult(context, leftSet, rightSet, ScKeynodes::nrel_sets_intersection);
+  if (resultSetAddr.IsValid())
+    return {context, resultSetAddr};
+
+  ScAddrToValueUnorderedMap<ScType> leftSetElements;
+  bool const leftSetIsValidForOperation = leftSet.GetElements(leftSetElements);
+  if (!leftSetIsValidForOperation)
+    SC_THROW_EXCEPTION(
+        utils::ExceptionInvalidParams,
+        "Not able to subtract provided sets because left set has outgoing both permanents and temporal sc-arcs.");
+
+  ScAddrToValueUnorderedMap<ScType> rightSetElements;
+  bool const rightSetIsValidForOperation = rightSet.GetElements(rightSetElements);
+  if (!rightSetIsValidForOperation)
+    SC_THROW_EXCEPTION(
+        utils::ExceptionInvalidParams,
+        "Not able to subtract provided sets because right set has outgoing both permanents and temporal sc-arcs.");
+
+  // Iterate over the left set and add elements to the result set if they are not in the right set
+  ScSet resultSet{context, context->GenerateNode(ScType::NodeConst)};
+  for (auto & [elementAddr, arcType] : leftSetElements)
+  {
+    ScType resultArcType = arcType;
+    auto const & it = rightSetElements.find(elementAddr);
+    if (it != rightSetElements.cend())
+    {
+      ScType const & otherArcType = it->second;
+      resultArcType = ScSet::GetResultAccessArcType(arcType, otherArcType, ScSet::SUBTRACT_SETS_ID)
+                      | ScSet::GetPermanencyAccessArcSubtype(arcType);
+
+      rightSetElements.erase(elementAddr);
+    }
+
+    resultSet.Append(elementAddr, resultArcType);
+  }
+
+  for (auto & [elementAddr, _] : rightSetElements)
+    resultSet.Append(elementAddr, ScType::EdgeAccessConstNegPerm);
+
+  ScSet::GenerateBinaryOperationResult(context, leftSet, rightSet, ScKeynodes::nrel_sets_intersection, resultSet);
+
+  return resultSet;
+}
+
+ScSet const operator-=(ScSet const & leftSet, ScSet const & rightSet)
+{
+  return leftSet - rightSet;
+}
+
+bool operator==(ScSet const & leftSet, ScSet const & rightSet)
+{
+  ScMemoryContext * context = leftSet.m_context;
+
+  ScTemplate resultTempl;
+  resultTempl.Quintuple(
+      leftSet,
+      ScType::EdgeUCommonVar,
+      rightSet,
+      ScType::EdgeAccessVar >> "_access_arc",
+      ScKeynodes::nrel_sets_equality);
+  ScTemplateSearchResult searchResult;
+  if (context->HelperSearchTemplate(resultTempl, searchResult))
+    return context->GetElementType(searchResult[0]["_access_arc"]).BitAnd(ScType::EdgeAccessConstPos)
+           == ScType::EdgeAccessConstPos;
+
+  ScAddrToValueUnorderedMap<ScType> leftSetElements;
+  bool const leftSetIsValidForOperation = leftSet.GetElements(leftSetElements);
+  if (!leftSetIsValidForOperation)
+    SC_THROW_EXCEPTION(
+        utils::ExceptionInvalidParams,
+        "Not able to compare provided sets because left set has outgoing both permanents and temporal sc-arcs.");
+
+  ScAddrToValueUnorderedMap<ScType> rightSetElements;
+  bool const rightSetIsValidForOperation = rightSet.GetElements(rightSetElements);
+  if (!rightSetIsValidForOperation)
+    SC_THROW_EXCEPTION(
+        utils::ExceptionInvalidParams,
+        "Not able to compare provided sets because right set has outgoing both permanents and temporal sc-arcs.");
+
+  bool isEqual = false;
+  if (leftSetElements.size() == rightSetElements.size())
+  {
+    bool result = true;
+    for (auto & [elementAddr, arcType] : leftSetElements)
+    {
+      auto const & it = rightSetElements.find(elementAddr);
+      if (it == rightSetElements.cend() || it->second != arcType)
+      {
+        result = false;
+        break;
+      }
+    }
+
+    if (result)
+      isEqual = true;
+  }
+
+  resultTempl.Clear();
+  resultTempl.Quintuple(
+      leftSet,
+      ScType::EdgeUCommonVar,
+      rightSet,
+      isEqual ? ScType::EdgeAccessVarPosPerm : ScType::EdgeAccessVarNegPerm,
+      ScKeynodes::nrel_sets_equality);
+  ScTemplateGenResult genResult;
+  context->HelperGenTemplate(resultTempl, genResult);
+
+  return isEqual;
+}
+
+void ScSet::GetBinaryOperationResultTemplate(
+    ScAddr const & leftSet,
+    ScAddr const & rightSet,
+    ScAddr const & relationAddr,
+    ScAddr const & resultSetAddr,
+    ScTemplate & resultTemplate)
+{
+  ScType connectorType = ScType::EdgeUCommonVar;
+  if (relationAddr == ScKeynodes::nrel_sets_difference)
+    connectorType = ScType::EdgeDCommonVar;
+
+  resultTemplate.Clear();
+  resultTemplate.Triple(leftSet, connectorType >> "_pair", rightSet);
+  resultTemplate.Quintuple(
+      "_pair",
+      ScType::EdgeDCommonVar,
+      resultSetAddr.IsValid() ? (resultSetAddr >> "_operation_result") : (ScType::NodeVar >> "_operation_result"),
+      ScType::EdgeAccessVarPosPerm,
+      relationAddr);
+}
+
+ScAddr ScSet::SearchBinaryOperationResult(
+    ScMemoryContext * context,
+    ScAddr const & leftSet,
+    ScAddr const & rightSet,
+    ScAddr const & relationAddr,
+    ScAddr const & resultSetAddr)
+{
+  ScTemplate resultTemplate;
+  GetBinaryOperationResultTemplate(leftSet, rightSet, relationAddr, resultSetAddr, resultTemplate);
+
+  ScTemplateSearchResult searchResult;
+  if (context->HelperSearchTemplate(resultTemplate, searchResult))
+    return searchResult[0]["_operation_result"];
+
+  return ScAddr::Empty;
+}
+
+ScAddr ScSet::GenerateBinaryOperationResult(
+    ScMemoryContext * context,
+    ScAddr const & leftSet,
+    ScAddr const & rightSet,
+    ScAddr const & relationAddr,
+    ScAddr const & resultSetAddr)
+{
+  ScTemplate resultTemplate;
+  GetBinaryOperationResultTemplate(leftSet, rightSet, relationAddr, resultSetAddr, resultTemplate);
+
+  ScTemplateGenResult genResult;
+  context->HelperGenTemplate(resultTemplate, genResult);
+  return genResult["_operation_result"];
+}
+
+bool ScSet::GetElements(ScAddrToValueUnorderedMap<ScType> & elements) const
+{
+  bool setHasPermArc = false;
+  bool setHasTempArc = false;
+  ScIterator3Ptr const iterator3 = m_context->Iterator3(*this, ScType::EdgeAccessConst, ScType::Unknown);
+  while (iterator3->Next())
+  {
+    ScAddr const & elementAddr = iterator3->Get(2);
+    ScType const & arcType = m_context->GetElementType(iterator3->Get(1));
+
+    if (arcType.BitAnd(ScType::EdgeAccessConstPerm) == ScType::EdgeAccessConstPerm)
+      setHasPermArc = true;
+
+    if (arcType.BitAnd(ScType::EdgeAccessConstTemp) == ScType::EdgeAccessConstTemp)
+      setHasTempArc = true;
+
+    elements.insert({elementAddr, arcType});
+  }
+
+  return !(setHasPermArc && setHasTempArc);
+}
+
+ScType ScSet::GetResultAccessArcType(ScType const & arcType, ScType const & otherArcType, size_t operationId)
+{
+  auto const & resultArcTypeIt =
+      ScSet::m_setsAccessArcTypesToUnionAndInteractionResultAccessArcTypes.find({arcType, otherArcType});
+  if (resultArcTypeIt != ScSet::m_setsAccessArcTypesToUnionAndInteractionResultAccessArcTypes.cend())
+    return resultArcTypeIt->second[operationId];
+  else
+    return ScType::EdgeAccessConst;
+}
+
+ScType ScSet::GetPermanencyAccessArcSubtype(ScType const & arcType)
+{
+  if (arcType.BitAnd(ScType::EdgeAccessConstPerm) == ScType::EdgeAccessConstPerm)
+    return ScType::EdgeAccessConstPerm;
+  else if (arcType.BitAnd(ScType::EdgeAccessConstTemp) == ScType::EdgeAccessConstTemp)
+    return ScType::EdgeAccessConstTemp;
+
+  return ScType::EdgeAccessConst;
+>>>>>>> 23d5bcb2 ([memory][set] Implement union, intersection, subtraction and equality of sets)
 }
--- sc-memory/sc-memory/sc_set.hpp
+++ sc-memory/sc-memory/sc_set.hpp
@@ -7,42 +7,167 @@
 #pragma once
 
 #include "sc_addr.hpp"
+#include "sc_type.hpp"
 
 #include "sc_utils.hpp"
 
-class ScSet : public ScAddr
+class ScTemplate;
+
+/*!
+ * @class ScSet
+ * @brief Represents a set of sc-elements in the sc-memory.
+ *
+ * The `ScSet` class provides an abstraction for working with sets of sc-elements in the sc-memory. It allows appending and
+ * removing sc-elements, checking their presence, and performing set operations such as union, intersection, and
+ * subtraction.
+ *
+ * @note All methods in this class assume that the set is valid and contains only sc-elements with the same sc-arc type
+ * (either all permanent or all temporary).
+ *
+ * @note If a set contains elements with both permanent and temporary sc-arcs, most operations will fail with an exception.
+ */
+class _SC_EXTERN ScSet : public ScAddr
 {
   friend class ScAgentContext;
+
+  struct ScTypeHashFunc;
+  struct ScTypeEqualFunc;
 
 public:
   _SC_EXTERN ~ScSet();
 
-  /* Append element into sc-hash-map. If element already exist, then doesn't append it and return false; otherwise
-   * returns true. */
-  _SC_EXTERN bool Append(ScAddr const & elAddr);
-  _SC_EXTERN bool Append(ScAddr const & elAddr, ScAddr const & attrAddr);
+  /*!
+   * @brief Appends a sc-element to the set with specified sc-arc type.
+   * @param elAddr A sc-address of sc-element to append.
+   * @param arcType A sc-arc type to use for appending sc-element.
+   * @return `true` if the sc-element was successfully appended, `false` otherwise.
+   */
+  _SC_EXTERN bool Append(ScAddr const & elAddr, ScType const & arcType = ScType::EdgeAccessConstPosPerm);
 
-  /* Remove element from sc-structure and returns true, if element removed. */
-  _SC_EXTERN bool Remove(ScAddr const & elAddr);
+  /*!
+   * @brief Appends an element to the set with a specified sc-arc type and an attribute (role relation).
+   * @param elAddr A sc-address of sc-element to append.
+   * @param attrAddr A sc-address of the attribute to associate with sc-element.
+   * @param arcType A sc-arc type to use for appending sc-element.
+   * @return `true` if the sc-element was successfully appended, `false` otherwise.
+   */
+  _SC_EXTERN bool Append(
+      ScAddr const & elAddr,
+      ScAddr const & attrAddr,
+      ScType const & arcType = ScType::EdgeAccessConstPosPerm);
 
-  /* Operator equal to append */
+  /*!
+   * @brief Removes a sc-element from set with specified sc-arc type.
+   * @param elAddr A sc-address of sc-element to remove.
+   * @return `true` if the sc-element was successfully removed, `false` otherwise.
+   */
+  _SC_EXTERN bool Remove(ScAddr const & elAddr, ScType const & arcType = ScType::EdgeAccessPos);
+
   _SC_EXTERN ScSet & operator<<(ScAddr const & elAddr);
-  _SC_EXTERN ScSet & operator<<(class ScTemplateResultItem const & res);
+  _SC_EXTERN ScSet & operator<<(class ScTemplateResultItem const & searchResultItem);
 
   /* Operator equal to remove */
   _SC_EXTERN ScSet & operator>>(ScAddr const & elAddr);
 
-  /* Check if specified element exist in set */
-  _SC_EXTERN bool HasElement(ScAddr const & elAddr) const;
+  /*!
+   * @brief Checks if a sc-element is present in set with specified arc type.
+   * @param elAddr A sc-address of sc-element to check.
+   * @param arcType A sc-arc type to use for checking sc-element's presence.
+   * @return `true` if the sc-element is present in the set with the specified arc type, `false` otherwise.
+   */
+  _SC_EXTERN bool Has(ScAddr const & elAddr, ScType const & arcType = ScType::EdgeAccessPos) const;
 
-  /* Check if set has no elements */
+  /*!
+   * @brief Checks if the set is empty.
+   * @return `true` if the set is empty, `false` otherwise.
+   */
   _SC_EXTERN bool IsEmpty() const;
 
-  /// TODO: implement +, -, == operators
+  /*!
+   * @brief Gets a power (size) of the set.
+   * @return A number of sc-elements in the set.
+   */
+  _SC_EXTERN size_t GetPower() const;
+
+  /*!
+   * @brief Performs the union operation with another set.
+   * @param otherSet A set to perform the union with.
+   * @return A resulting set after the union operation.
+   * @throw utils::ExceptionInvalidParams if either set contains elements with both permanent and temporary arcs.
+   */
+  _SC_EXTERN ScSet & Unite(ScSet const & otherSet);
+
+  /*!
+   * @brief Performs the intersection operation with another set.
+   * @param otherSet A set to perform the intersection with.
+   * @return A resulting set after the intersection operation.
+   * @throw utils::ExceptionInvalidParams if either set contains elements with both permanent and temporary arcs.
+   */
+  _SC_EXTERN ScSet & Intersect(ScSet const & otherSet);
+
+  /*!
+   * @brief Performs the difference operation with another set.
+   * @param otherSet A set to perform the difference with.
+   * @return A resulting set after the difference operation.
+   * @throw utils::ExceptionInvalidParams if either set contains elements with both permanent and temporary arcs.
+   */
+  _SC_EXTERN ScSet & Subtract(ScSet const & otherSet);
+
+  _SC_EXTERN friend ScSet const operator+(ScSet const & leftSet, ScSet const & rightSet);
+
+  _SC_EXTERN friend ScSet const operator+=(ScSet const & leftSet, ScSet const & rightSet);
+
+  _SC_EXTERN friend ScSet const operator*(ScSet const & leftSet, ScSet const & rightSet);
+
+  _SC_EXTERN friend ScSet const operator*=(ScSet const & leftSet, ScSet const & rightSet);
+
+  _SC_EXTERN friend ScSet const operator-(ScSet const & leftSet, ScSet const & rightSet);
+
+  _SC_EXTERN friend ScSet const operator-=(ScSet const & leftSet, ScSet const & rightSet);
+
+  _SC_EXTERN friend bool operator==(ScSet const & leftSet, ScSet const & rightSet);
 
 protected:
+  static std::unordered_map<std::pair<ScType, ScType>, std::array<ScType, 3>, ScTypeHashFunc, ScTypeEqualFunc>
+      m_setsAccessArcTypesToUnionAndInteractionResultAccessArcTypes;
+  static size_t const UNITE_SETS_ID;
+  static size_t const INTERSECT_SETS_ID;
+  static size_t const SUBTRACT_SETS_ID;
+
+  /*!
+   * @brief Constructs a new `ScSet` instance.
+   * @param context A sc-memory context in which the set resides.
+   * @param setAddr A sc-address of set in the sc-memory.
+   */
   _SC_EXTERN ScSet(class ScMemoryContext * context, ScAddr const & setAddr);
 
 private:
   ScMemoryContext * m_context;
+
+  static void GetBinaryOperationResultTemplate(
+      ScAddr const & leftSet,
+      ScAddr const & rightSet,
+      ScAddr const & relationAddr,
+      ScAddr const & resultSetAddr,
+      ScTemplate & resultTemplate);
+
+  static ScAddr SearchBinaryOperationResult(
+      ScMemoryContext * context,
+      ScAddr const & leftSet,
+      ScAddr const & rightSet,
+      ScAddr const & relationAddr,
+      ScAddr const & resultSetAddr = ScAddr::Empty);
+
+  static ScAddr GenerateBinaryOperationResult(
+      ScMemoryContext * context,
+      ScAddr const & leftSet,
+      ScAddr const & rightSet,
+      ScAddr const & relationAddr,
+      ScAddr const & resultSetAddr = ScAddr::Empty);
+
+  bool GetElements(ScAddrToValueUnorderedMap<ScType> & elements) const;
+
+  static ScType GetResultAccessArcType(ScType const & arcType, ScType const & otherArcType, size_t operationId);
+
+  static ScType GetPermanencyAccessArcSubtype(ScType const & arcType);
 };
