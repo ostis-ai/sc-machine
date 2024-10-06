823e66c23c27c0e7b86c4c32b20b209960e202ea
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-10-06 15:21:58 +0300

    [scripts][cmake] Install libxml2

--- scripts/install_deps_macOS.sh
+++ scripts/install_deps_macOS.sh
@@ -6,6 +6,6 @@ then
   source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)/set_vars.sh"
 fi
 
-brew install glib antlr antlr4-cpp-runtime temurin pkgconfig cmake ninja ccache asio websocketpp nlohmann-json
+brew install glib antlr antlr4-cpp-runtime temurin pkgconfig cmake ninja ccache asio websocketpp nlohmann-json libxml2
 
 "${SC_MACHINE_PATH}/scripts/install_deps_python.sh"
