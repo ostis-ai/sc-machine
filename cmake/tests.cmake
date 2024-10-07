656e3c839062f1633bb8c6f552eee30e09e49e04
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-10-07 15:09:48 +0300

    [cmake] Fix install path for extensions

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -1,21 +1,20 @@
 function(make_tests_from_folder folder)
-
-  set (SINGLE_ARGS NAME)
-  set (MULTI_ARGS DEPENDS INCLUDES ARGUMENTS)
+  set(SINGLE_ARGS NAME)
+  set(MULTI_ARGS DEPENDS INCLUDES ARGUMENTS)
 
   cmake_parse_arguments(TEST "" "${SINGLE_ARGS}" "${MULTI_ARGS}" ${ARGN})
 
-  set (target "${TEST_NAME}")
+  set(target "${TEST_NAME}")
 
   message(STATUS "Create test ${target}")
 
-  file (GLOB_RECURSE SOURCES 
-        "${folder}/*.cpp"
-        "${folder}/*.hpp")
+  file(GLOB_RECURSE SOURCES 
+      "${folder}/*.cpp"
+      "${folder}/*.hpp"
+  )
 
   add_executable(${target} ${SOURCES})
-  target_link_libraries(${target} GTest::gtest GTest::gtest_main ${TEST_DEPENDS})
+  target_link_libraries(${target} GTest::gtest_main ${TEST_DEPENDS})
   target_include_directories(${target} PRIVATE ${TEST_INCLUDES})
   gtest_discover_tests(${target} TEST_LIST ${target_tests})
-
 endfunction()
