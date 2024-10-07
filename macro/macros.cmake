d5c8a913b408738d95cc61966cd25488b7c2fa7d
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-10-07 15:25:56 +0300

    [cmake] Remove duplicating project version

--- macro/macros.cmake
+++ macro/macros.cmake
@@ -1,4 +1,4 @@
-include(${CMAKE_CURRENT_LIST_DIR}/project_version.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/parse_project_version.cmake)
 
 include(${CMAKE_CURRENT_LIST_DIR}/code_coverage.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/sanitizer.cmake)
