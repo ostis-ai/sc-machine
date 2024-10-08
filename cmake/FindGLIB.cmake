c089cdbbab2b7db19eed18a4af6c92cbb5fa036d
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-10-08 11:27:54 +0300

    [cmake] Cache find glib

--- cmake/FindGLIB.cmake
+++ cmake/FindGLIB.cmake
@@ -1,18 +1,27 @@
-macro(FindGLIB)
+function(FindGLIB)
+    if (glib_CACHED)
+        message(STATUS "GLib found in cache.")
+        return()
+    endif()
+
     find_package(glib QUIET)
 
     if (NOT glib_FOUND)
-        message("Trying to use system-installed glib")
+        message(STATUS "Trying to use system-installed glib")
 
         find_package(PkgConfig REQUIRED)
         pkg_search_module(GLIB2 REQUIRED glib-2.0)
         pkg_search_module(GLIB2_MODULE REQUIRED gmodule-2.0)
 
         if (NOT GLIB2_FOUND OR NOT GLIB2_MODULE_FOUND)
-            error("glib2 not found!")
+            message(FATAL_ERROR "glib2 not found!")
         endif()
 
-        set(glib_INCLUDE_DIRS ${GLIB2_INCLUDE_DIRS} ${GLIB2_MODULE_INCLUDE_DIRS})
-        set(glib_LIBRARIES ${GLIB_LDFLAGS} ${GLIB2_MODULE_LDFLAGS})
+        set(glib_INCLUDE_DIRS ${GLIB2_INCLUDE_DIRS} ${GLIB2_MODULE_INCLUDE_DIRS} CACHE STRING "Include directories for GLib")
+        set(glib_LIBRARIES ${GLIB_LDFLAGS} ${GLIB2_MODULE_LDFLAGS} CACHE STRING "Libraries for GLib")
+        
+        set(glib_CACHED TRUE CACHE BOOL "GLib found")
+    else()
+        message(STATUS "GLib is already found.")
     endif()
-endmacro()
+endfunction()
