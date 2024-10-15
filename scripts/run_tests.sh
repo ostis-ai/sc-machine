436b66cd21510df830d8c30f6352c50d8a5ecbd5
FallenChromium <fallenchromium@gmail.com>
2024-10-15 03:47:23 +0300

    [build][ci] Get rid of CMake script wrappers

--- scripts/run_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eo pipefail
-
-if [[ -z "${BINARY_PATH}" || -z "${BUILD_PATH}" || -z "${SC_MACHINE_PATH}" ]];
-then
-  source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)/set_vars.sh"
-fi
-
-cd "${BUILD_PATH}" && ctest -C Debug -V
