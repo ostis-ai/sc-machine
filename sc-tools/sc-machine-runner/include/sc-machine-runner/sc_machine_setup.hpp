a037ca4b89e97a2de94cbbee6d34ef47ad7e3c30
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-10-29 00:58:51 +0300

    [cmake][tools][kpm] Fix includes

---
+++ sc-tools/sc-machine-runner/include/sc-machine-runner/sc_machine_setup.hpp
@@ -0,0 +1,13 @@
+/*
+ * This source file is part of an OSTIS project. For the latest info, see http://ostis.net
+ * Distributed under the MIT License
+ * (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
+ */
+
+#pragma once
+
+#include <sc-memory/sc_type.hpp>
+
+void PrintStartMessage();
+
+sc_int BuildAndRunMachine(sc_int argc, sc_char * argv[]);
