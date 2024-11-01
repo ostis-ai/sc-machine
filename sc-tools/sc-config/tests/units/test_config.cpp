783fdfa8fd86339d44e5ac41b204412d974387e2
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-11-01 20:16:55 +0300

    [tools][config] Test config; deprecated and removed options

---
+++ sc-tools/sc-config/tests/units/test_config.cpp
@@ -0,0 +1,87 @@
+/*
+ * This source file is part of an OSTIS project. For the latest info, see http://ostis.net
+ * Distributed under the MIT License
+ * (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
+ */
+
+#include <gtest/gtest.h>
+
+#include "test_defines.hpp"
+
+#include <sc-config/sc_config.hpp>
+#include <sc-config/sc_memory_config.hpp>
+
+TEST(ScConfig, ValidConfig)
+{
+  ScConfig config{SC_MACHINE_INI};
+
+  ScMemoryConfig memoryConfig{config, {}};
+
+  sc_memory_params const params = memoryConfig.GetParams();
+  EXPECT_EQ(params.max_loaded_segments, 1000u);
+  EXPECT_EQ(params.dump_memory, SC_TRUE);
+  EXPECT_EQ(params.dump_memory_period, 4u);
+  EXPECT_EQ(params.dump_memory_statistics, SC_TRUE);
+  EXPECT_EQ(params.dump_memory_statistics_period, 4u);
+  EXPECT_EQ(std::string(params.log_type), "Console");
+  EXPECT_EQ(std::string(params.log_file), "");
+  EXPECT_EQ(std::string(params.log_level), "Debug");
+  EXPECT_EQ(std::string(params.binaries), "sc-machine-test-repo");
+  EXPECT_EQ(std::string(params.binaries), "sc-machine-test-repo");
+}
+
+TEST(ScConfig, RemoveSavePeriod)
+{
+  ScConfig config{SC_CONFIGS "/removed-save-period.ini"};
+
+  ScMemoryConfig memoryConfig{config, {}};
+
+  EXPECT_THROW(memoryConfig.GetParams(), utils::ExceptionInvalidParams);
+}
+
+TEST(ScConfig, RemoveUpdatePeriod)
+{
+  ScConfig config{SC_CONFIGS "/removed-update-period.ini"};
+
+  ScMemoryConfig memoryConfig{config, {}};
+
+  EXPECT_THROW(memoryConfig.GetParams(), utils::ExceptionInvalidParams);
+}
+
+TEST(ScConfig, DeprecatedRepoPath)
+{
+  ScConfig config{SC_CONFIGS "/deprecated-repo-path.ini"};
+
+  ScMemoryConfig memoryConfig{config, {}};
+
+  sc_memory_params const params = memoryConfig.GetParams();
+  EXPECT_EQ(params.max_loaded_segments, 1000u);
+  EXPECT_EQ(params.dump_memory, SC_TRUE);
+  EXPECT_EQ(params.dump_memory_period, 4u);
+  EXPECT_EQ(params.dump_memory_statistics, SC_TRUE);
+  EXPECT_EQ(params.dump_memory_statistics_period, 4u);
+  EXPECT_EQ(std::string(params.log_type), "Console");
+  EXPECT_EQ(std::string(params.log_file), "");
+  EXPECT_EQ(std::string(params.log_level), "Debug");
+  EXPECT_EQ(std::string(params.binaries), "sc-machine-test-repo");
+  EXPECT_EQ(std::string(params.binaries), "sc-machine-test-repo");
+}
+
+TEST(ScConfig, DeprecatedExtensionsPath)
+{
+  ScConfig config{SC_CONFIGS "/deprecated-extensions-path.ini"};
+
+  ScMemoryConfig memoryConfig{config, {}};
+
+  sc_memory_params const params = memoryConfig.GetParams();
+  EXPECT_EQ(params.max_loaded_segments, 1000u);
+  EXPECT_EQ(params.dump_memory, SC_TRUE);
+  EXPECT_EQ(params.dump_memory_period, 4u);
+  EXPECT_EQ(params.dump_memory_statistics, SC_TRUE);
+  EXPECT_EQ(params.dump_memory_statistics_period, 4u);
+  EXPECT_EQ(std::string(params.log_type), "Console");
+  EXPECT_EQ(std::string(params.log_file), "");
+  EXPECT_EQ(std::string(params.log_level), "Debug");
+  EXPECT_EQ(std::string(params.binaries), "sc-machine-test-repo");
+  EXPECT_EQ(std::string(params.binaries), "sc-machine-test-repo");
+}
