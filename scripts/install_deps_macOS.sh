70222dfe6cec859ba51593caf253db342bbd3659
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-11-22 21:33:17 +0300

    [scripts][macOS] Lib pkgconfig is installed by default

--- scripts/install_deps_macOS.sh
+++ scripts/install_deps_macOS.sh
@@ -6,6 +6,6 @@ then
   source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)/set_vars.sh"
 fi
 
-brew install glib antlr antlr4-cpp-runtime temurin pkgconfig cmake ninja ccache asio websocketpp nlohmann-json libxml2
+brew install glib antlr antlr4-cpp-runtime temurin cmake ninja ccache asio websocketpp nlohmann-json libxml2
 
 "${SC_MACHINE_PATH}/scripts/install_deps_python.sh"
