f97767ee931bfaf4b73cfd74ae4969f405d2852a
IRomanchuk06 <iromanchuk06@gmail.com>
2025-04-03 19:57:43 +0300

    Linear version history

--- sc-memory/sc-core/src/sc-store/sc-transaction/sc_element_version.h
+++ sc-memory/sc-core/src/sc-store/sc-transaction/sc_element_version.h
@@ -3,22 +3,6 @@
 
 #include <sc-core/sc_types.h>
 
-typedef struct sc_element_version
-{
-  sc_element* data;
-  sc_uint64 version_id;
-  struct sc_element_version * prev_version;
-  struct sc_element_version * next_version;
-  sc_bool is_committed;
-  sc_type modified_fields;
-} sc_element_version;
-
-typedef struct sc_version_chain
-{
-  sc_element_version * head;
-  sc_element_version * tail;
-} sc_version_chain;
-
 typedef enum
 {
   SC_ELEMENT_FLAGS_MODIFIED = 1 << 0,
@@ -27,6 +11,26 @@ typedef enum
   SC_ELEMENT_LINKS_MODIFIED = 1 << 3
 } SC_ELEMENT_MODIFIED_FLAGS;
 
-sc_element_version * sc_element_create_new_version(sc_element * element, sc_uint64 version_id, sc_type modified_fields);
+typedef struct sc_element_version
+{
+  const sc_element* data;
+  sc_uint64 version_id;
+  sc_uint64 transaction_id;
+  struct sc_element_version *parent_version;
+  sc_bool is_committed;
+  SC_ELEMENT_MODIFIED_FLAGS modified_fields;
+} sc_element_version;
+
+typedef struct sc_version_history
+{
+  sc_element_version *latest_version;
+} sc_version_history;
+
+sc_element_version * sc_element_create_new_version(
+  sc_element_version *parent,
+  const sc_element *new_element_data,
+  sc_uint64 version_id,
+  sc_uint64 transaction_id,
+  SC_ELEMENT_MODIFIED_FLAGS modified_fields);
 
 #endif
