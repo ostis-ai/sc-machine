ee8c5700c4e1281a2fb2dd6c243112422fe81af9
NikitaZotov <nikita.zotov.belarus@gmail.com>
2025-05-01 12:38:05 +0300

    [scripts] Add link to PR in TODO about pinning asio version

--- scripts/install_deps_macOS.sh
+++ scripts/install_deps_macOS.sh
@@ -5,7 +5,9 @@ SCRIPTS_PATH="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
 
 brew install glib temurin pkgconfig cmake ninja ccache websocketpp nlohmann-json libxml2 googletest google-benchmark
 
-# TODO(NikitaZotov): websocketpp 0.8.2 supports asio 1.30.2 only. Remove it after releasing new version of websocketpp.
+# TODO(NikitaZotov): Remove asio version 1.30.2 pinning below.
+# Required because websocketpp 0.8.2 is incompatible with newer asio.
+# Remove after updating websocketpp to a version including the fix from PR https://github.com/zaphoyd/websocketpp/pull/1164 or later.
 brew uninstall --ignore-dependencies asio
 curl -O https://raw.githubusercontent.com/Homebrew/homebrew-core/502489d3a4c1ca0a3854830eb5da2327b6feb54d/Formula/a/asio.rb
 brew install asio.rb
