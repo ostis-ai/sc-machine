07203c84f824605d12827acff5e1095dc3dc56a4
NikitaZotov <nikita.zotov.belarus@gmail.com>
2025-05-01 12:17:22 +0300

    [scripts] Pin asio version on macOS

--- scripts/install_deps_macOS.sh
+++ scripts/install_deps_macOS.sh
@@ -3,6 +3,9 @@ set -eo pipefail
 
 SCRIPTS_PATH="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
 
-brew install glib temurin pkgconfig cmake ninja ccache asio websocketpp nlohmann-json libxml2 googletest google-benchmark
+brew install glib temurin pkgconfig cmake ninja ccache websocketpp nlohmann-json libxml2 googletest google-benchmark
+
+curl -O https://raw.githubusercontent.com/Homebrew/homebrew-core/502489d3a4c1ca0a3854830eb5da2327b6feb54d/Formula/a/asio.rb
+brew install asio.rb
 
 "${SCRIPTS_PATH}/install_deps_python.sh"
