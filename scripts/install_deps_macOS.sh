41f0dbaa1826c0e97bce28e1b834bb3205899f5f
NikitaZotov <nikita.zotov.belarus@gmail.com>
2025-05-01 12:30:55 +0300

    [scripts] Ignore dependencies during uninstalling asio

--- scripts/install_deps_macOS.sh
+++ scripts/install_deps_macOS.sh
@@ -5,8 +5,8 @@ SCRIPTS_PATH="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
 
 brew install glib temurin pkgconfig cmake ninja ccache websocketpp nlohmann-json libxml2 googletest google-benchmark
 
-# TODO(NikitaZotov): websocketpp 0.8.2 support asio 1.30.2 only. Remove it after releasing new version of websocketpp.
-brew uninstall asio
+# TODO(NikitaZotov): websocketpp 0.8.2 supports asio 1.30.2 only. Remove it after releasing new version of websocketpp.
+brew uninstall --ignore-dependencies asio
 curl -O https://raw.githubusercontent.com/Homebrew/homebrew-core/502489d3a4c1ca0a3854830eb5da2327b6feb54d/Formula/a/asio.rb
 brew install asio.rb
 brew pin asio
